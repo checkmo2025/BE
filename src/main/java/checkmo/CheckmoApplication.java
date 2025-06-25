e85626d20a7d32c11412f076d1c08479df0d44e1
MODUGGAGI <kiroro0814@naver.com>
2025-06-25 23:00:43 +0900

    feat : 프로젝트 기본 설정

---
+++ src/main/java/checkmo/CheckmoApplication.java
@@ -0,0 +1,13 @@
+package checkmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckmoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckmoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/checkmo/CheckmoApplicationTests.java
@@ -0,0 +1,13 @@
+package checkmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckmoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
