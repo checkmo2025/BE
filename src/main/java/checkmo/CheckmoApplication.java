d00c0b057ab262744fa287712ed75486298d1a9e
MODUGGAGI <kiroro0814@naver.com>
2025-07-08 22:52:23 +0900

    ✨feat: Redis 자동 설정 비활성화를 위한 클래스 제외 처리

--- src/main/java/checkmo/CheckmoApplication.java
+++ src/main/java/checkmo/CheckmoApplication.java
@@ -2,9 +2,14 @@ package checkmo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                RedisRepositoriesAutoConfiguration.class
+        }
+)
 @EnableJpaAuditing
 public class CheckmoApplication {
 
