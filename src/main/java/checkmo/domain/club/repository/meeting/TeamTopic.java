4d021869952181080c95d4ca444c7686cf0dd5e2
MODUGGAGI <kiroro0814@naver.com>
2025-07-08 22:30:47 +0900

    ✨feat: TeamTopicRepository 인터페이스 이름 수정
    기존에는 TeamTopic으로 잘못되어있었음

--- src/main/java/checkmo/domain/club/repository/meeting/TeamTopic.java
+++
@@ -1,6 +0,0 @@
-package checkmo.domain.club.repository.meeting;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamTopic extends JpaRepository<TeamTopic, Long> {
-}
---
+++ src/main/java/checkmo/domain/club/repository/meeting/TeamTopicRepository.java
@@ -0,0 +1,7 @@
+package checkmo.domain.club.repository.meeting;
+
+import checkmo.domain.club.entity.meeting.TeamTopic;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamTopicRepository extends JpaRepository<TeamTopic, Long> {
+}
