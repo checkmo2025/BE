642692a26989a6a22d337f5c56735e2d4230f352
MODUGGAGI <kiroro0814@naver.com>
2025-07-10 20:49:14 +0900

    ✨feat: CategoryAssignmentCommandService 메소드 리팩토링
    회원 및 모임의 관심 카테고리 추가/제거 메소드를 수정 메소드로 통합.

--- src/main/java/checkmo/domain/category/service/command/CategoryAssignmentCommandService.java
+++ src/main/java/checkmo/domain/category/service/command/CategoryAssignmentCommandService.java
@@ -10,38 +10,20 @@ import java.util.List;
  */
 public interface CategoryAssignmentCommandService {
     /**
-     * 회원의 관심 카테고리 설정
+     * 회원의 관심 카테고리 수정 - 이미 추가되어있는 관심 카테고리는 서비스 로직 구현 시 제외하고 새로운 카테고리만 추가 or 제거
      *
      * @param memberId     회원 ID
      * @param request  추가할 카테고리 ID 목록
      * @return 추가된 카테고리 정보가 담긴 DTO
      */
-    CategoryResponseDTO.CategoryListResponseDTO addCategoriesToMember(Long memberId, CategoryRequestDTO.CategoryListRequestDTO request);
+    CategoryResponseDTO.CategoryListResponseDTO modifyCategoriesToMember(Long memberId, CategoryRequestDTO.CategoryListRequestDTO request);
 
     /**
-     * 회원의 관심 카테고리 제거
-     *
-     * @param memberId     회원 ID
-     * @param request  제거할 카테고리 ID 목록
-     * @return 제거되고 남아있는 카테고리 정보가 담긴 DTO
-     */
-    CategoryResponseDTO.CategoryListResponseDTO removeCategoriesFromMember(Long memberId, CategoryRequestDTO.CategoryListRequestDTO request);
-
-    /**
-     * Club의 관심 카테고리 설정
+     * Club의 관심 카테고리 수정 - 이미 추가되어있는 관심 카테고리는 서비스 로직 구현 시 제외하고 새로운 카테고리만 추가 or 제거
      *
      * @param clubId       모임 ID
      * @param request  추가할 카테고리 ID 목록
      * @return 추가된 카테고리 정보가 담긴 DTO
      */
-    CategoryResponseDTO.CategoryListResponseDTO addCategoriesToClub(Long clubId, CategoryRequestDTO.CategoryListRequestDTO request);
-
-    /**
-     * Club의 관심 카테고리 제거
-     *
-     * @param clubId       모임 ID
-     * @param request  제거할 카테고리 ID 목록
-     * @return 제거되고 남아있는 카테고리 정보가 담긴 DTO
-     */
-    CategoryResponseDTO.CategoryListResponseDTO removeCategoriesFromClub(Long clubId, CategoryRequestDTO.CategoryListRequestDTO request);
+    CategoryResponseDTO.CategoryListResponseDTO modifyCategoriesToClub(Long clubId, CategoryRequestDTO.CategoryListRequestDTO request);
 }
