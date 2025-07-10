13589a0491c741dde86ade2daa51db2e05b19aef
MODUGGAGI <kiroro0814@naver.com>
2025-07-10 16:48:55 +0900

    ✨feat: Notification관련 이벤트들 비동기 처리할 Handler 클래스 추가

---
+++ src/main/java/checkmo/domain/notification/handler/NotificationEventHandler.java
@@ -0,0 +1,32 @@
+package checkmo.domain.notification.handler;
+
+import checkmo.domain.notification.service.command.NotificationCommandService;
+import checkmo.event.FollowEvent;
+import checkmo.event.LikeEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+public class NotificationEventHandler {
+
+    //TODO : 나중에 구현할 때 주석 해제하고 사용하기!!
+    //private final NotificationCommandService notificationCommandService;
+
+    @Async("notificationExecutor")
+    @TransactionalEventListener
+    public void handleNotificationEvent(FollowEvent event) {
+        // 이벤트 처리 로직
+
+    }
+
+    @Async("notificationExecutor")
+    @TransactionalEventListener
+    public void handleNotificationEvent(LikeEvent event) {
+        // 이벤트 처리 로직
+
+    }
+
+}
