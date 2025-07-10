462fb75bb67d8f91c7595aa8058a9c69a8830250
MODUGGAGI <kiroro0814@naver.com>
2025-07-10 18:17:58 +0900

    ✨feat: MemberFollow 관련 서비스 인터페이스 메소드 추가
    MemberFollowCommandService 및 MemberFollowQueryService에 팔로우/언팔로우 및 팔로워/팔로잉 조회 관련 메소드 추가.

--- src/main/java/checkmo/domain/member/service/command/MemberFollowCommandService.java
+++ src/main/java/checkmo/domain/member/service/command/MemberFollowCommandService.java
@@ -7,4 +7,28 @@ package checkmo.domain.member.service.command;
  * 팔로우/팔로잉 시 NotificationCommandService를 통해 알림 처리
  */
 public interface MemberFollowCommandService {
+
+    /**
+     * 특정 회원을 팔로우
+     *
+     * @param memberId 팔로우할 회원의 ID
+     * @param targetNickname 팔로우 대상 회원의 nickname -> 서비스 로직에서 닉네임으로 회원의 ID를 조회하여 팔로우 처리
+     */
+    void followMember(String memberId, String targetNickname);
+
+    /**
+     * 특정 회원의 팔로우를 취소 (언팔로우)
+     *
+     * @param memberId 언팔로우할 회원의 ID
+     * @param targetNickname 팔로우 대상 회원의 nickname -> 서비스 로직에서 닉네임으로 회원의 ID를 조회하여 팔로우 처리
+     */
+    void unfollowMember(String memberId, String targetNickname);
+
+    /**
+     * 특정 회원의 팔로잉를 취소 (언팔로잉)
+     *
+     * @param memberId 언팔로우할 회원의 ID
+     * @param targetNickname 팔로우 대상 회원의 nickname -> 서비스 로직에서 닉네임으로 회원의 ID를 조회하여 팔로우 처리
+     */
+    void unfollowingMember(String memberId, String targetNickname);
 }
--- src/main/java/checkmo/domain/member/service/query/MemberFollowQueryService.java
+++ src/main/java/checkmo/domain/member/service/query/MemberFollowQueryService.java
@@ -1,9 +1,45 @@
 package checkmo.domain.member.service.query;
 
+import checkmo.domain.member.dto.MemberResponseDTO;
+
 /**
  * 팔로우/팔로잉 조회 서비스
  *
  * 팔로워/팔로잉 목록 조회
  */
 public interface MemberFollowQueryService {
+
+    /**
+     * 특정 회원의 팔로우 목록 전체 조회
+     *
+     * @param memberId 조회할 회원의 ID
+     * @param cursorId 커서 ID (페이징을 위한) => 커서로 사용되는 ID는 Follow 엔티티 자체의 ID 값으로 사용하기!!
+     * @return 팔로워 목록
+     */
+     MemberResponseDTO.FollowerListResponseDTO getFollowers(String memberId, Long cursorId);
+
+    /**
+     * 특정 회원의 팔로잉 목록 전체 조회
+     *
+     * @param memberId 조회할 회원의 ID
+     * @param cursorId 커서 ID (페이징을 위한) => 커서로 사용되는 ID는 Follow 엔티티 자체의 ID 값으로 사용하기!!
+     * @return 팔로잉 목록
+     */
+    MemberResponseDTO.FollowingListResponseDTO getFollowing(String memberId, Long cursorId);
+
+    /**
+     * 특정 회원의 팔로우 목록 size 개수만큼 조회
+     *
+     * @param memberId 조회할 회원의 ID
+     * @return 팔로워 목록
+     */
+    MemberResponseDTO.FollowerListResponseDTO getFollowers(Long memberId, int size);
+
+    /**
+     * 특정 회원의 팔로잉 목록 size 개수만큼 조회
+     *
+     * @param memberId 조회할 회원의 ID
+     * @return 팔로잉 목록
+     */
+    MemberResponseDTO.FollowingListResponseDTO getFollowing(Long memberId, int size);
 }
