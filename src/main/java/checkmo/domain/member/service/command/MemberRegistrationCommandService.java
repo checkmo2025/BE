a97e45c1ef382130d41f89d1f0ea360c6e5c49fa
MODUGGAGI <kiroro0814@naver.com>
2025-07-10 21:49:29 +0900

    ✨feat: MemberRegistrationCommandService 메소드 대폭 확장
    이메일 및 닉네임 중복 확인, 이메일 인증, 회원 가입, 추가 정보 입력 관련 메소드 추가.

--- src/main/java/checkmo/domain/member/service/command/MemberRegistrationCommandService.java
+++ src/main/java/checkmo/domain/member/service/command/MemberRegistrationCommandService.java
@@ -1,5 +1,8 @@
 package checkmo.domain.member.service.command;
 
+import checkmo.domain.member.dto.MemberRequestDTO;
+import checkmo.domain.member.dto.MemberResponseDTO;
+
 /**
  * 회원 가입 서비스
  *
@@ -8,11 +11,64 @@ package checkmo.domain.member.service.command;
  */
 public interface MemberRegistrationCommandService {
 
-    // 이메일 중복 확인 메소드 만들기
+    /**
+     * Redis Hash 구조 사용해서
+     * key : kiroro0814@naver.com
+     * field :
+     *      code : [인증 코드]
+     *      verified : true/false
+     * 이렇게 1개의 이메일 키 값에 두개의 값 저장
+     */
 
-    // 이메일 인증 요청 메소드 만들기
+    /**
+     * 이메일 중복 확인
+     *
+     * @param email 확인할 이메일
+     * @return 중복 여부 (true: 중복됨, false: 사용 가능)
+     */
+    boolean isEmailDuplicated(String email);
 
-    // 이메일 인증번호 확인 메소드 만들기
+    /**
+     * 닉네임 중복 확인
+     *
+     * @param nickname 확인할 닉네임
+     * @return 중복 여부 (true: 중복됨, false: 사용 가능)
+     */
+    boolean isNicknameDuplicated(String nickname);
 
-    // 회원 가입 메소드 만들기 (닉네임, 프로필 이미지, 관심 도서 분야 등) -> 얘는 소셜 로그인도 사용
+    /**
+     * 이메일 인증 요청
+     *
+     * @param email 인증할 이메일
+     */
+    void sendEmailVerification(String email);
+
+    /**
+     * 이메일 인증번호 확인
+     *
+     * @param request 이메일 인증 요청 DTO
+     * @return 인증 성공 여부
+     */
+    boolean verifyEmailCode(MemberRequestDTO.EmailVerificationRequestDTO request);
+
+    /**
+     * 회원 가입 - 이 정보를 전달받으면 Access Token, RefreshToken 생성
+     *  - 추가 정보 입력 안받으면 정식 회원 아님
+     *
+     * @param request id,pw DTO
+     * @return 회원 가입 응답 DTO
+     */
+    MemberResponseDTO.SignUpResponseDTO signUp(
+            MemberRequestDTO.SignUpRequestDTO request
+    );
+
+    /**
+     * 회원 추가 정보 입력
+     *
+     * @param request 추가 정보 DTO (닉네임, 프로필 이미지, 관심 카테고리)
+     * @return void -> 어차피 회원 프로필 정보 완료 후에는 메인 화면에 로그인된 상태로 리다이렉트
+     */
+    void addAdditionalInfo(
+            MemberRequestDTO.AdditionalInfoDTO request
+    );
 }
