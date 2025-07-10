d4db0a549e351f037245dec6a85c1049b7c5003a
MODUGGAGI <kiroro0814@naver.com>
2025-07-10 16:47:39 +0900

    ✨feat: NotificationCommandService, NotificationQueryService 인터페이스에 메소드 추가

--- src/main/java/checkmo/domain/notification/service/command/NotificationCommandService.java
+++ src/main/java/checkmo/domain/notification/service/command/NotificationCommandService.java
@@ -1,5 +1,7 @@
 package checkmo.domain.notification.service.command;
 
+import checkmo.domain.notification.dto.NotificationRequestDTO;
+
 /**
  * 알림 생성, 수정, 삭제 서비스
  *
@@ -8,4 +10,25 @@ package checkmo.domain.notification.service.command;
  * ex) 모임 공지사항, 팔로우 알림 등등
  */
 public interface NotificationCommandService {
+
+    /**
+     * 좋아요 알림 생성
+     *
+     * @param request 좋아요 알림 정보 DTO
+     */
+    void createNotification(NotificationRequestDTO.LikeRequestDTO request);
+
+    /**
+     * 팔로우(구독) 알림 생성
+     *
+     * @param request 팔로우 알림 정보 DTO
+     */
+     void createNotification(NotificationRequestDTO.FollowRequestDTO request);
+
+    /**
+     * 알림 읽음 처리
+     * @param notificationId 읽음 처리할 알림 ID
+     * @param memberId 읽음 처리할 회원 ID (receiverId)
+     */
+    void markNotificationAsRead(Long notificationId, String memberId);
 }
--- src/main/java/checkmo/domain/notification/service/query/NotificationQueryService.java
+++ src/main/java/checkmo/domain/notification/service/query/NotificationQueryService.java
@@ -1,5 +1,9 @@
 package checkmo.domain.notification.service.query;
 
+import checkmo.domain.notification.dto.NotificationResponseDTO;
+
+import java.util.List;
+
 /**
  * 알림 조회 서비스
  *
@@ -7,4 +11,19 @@ package checkmo.domain.notification.service.query;
  * ex) 모임 공지사항, 알림 목록 조회, 읽지 않은 알림 개수 확인 등등
  */
 public interface NotificationQueryService {
+    /**
+     * 알림 목록 조회
+     *
+     * @param memberId receiver ID
+     * @return 전체 알림 목록
+     */
+    List<NotificationResponseDTO.NotificationListResponseDTO> getAllNotifications(Long memberId);
+
+    /**
+     * 10개의 알림 목록 조회
+     *
+     * @param memberId receiver ID
+     * @return 알림 목록 10개
+     */
+    List<NotificationResponseDTO.NotificationListResponseDTO> getRecentNotifications(Long memberId);
 }
