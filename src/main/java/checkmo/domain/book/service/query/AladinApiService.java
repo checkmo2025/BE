77d2fc6d60951f3f6f485727faadaa6ae90fac37
MODUGGAGI <kiroro0814@naver.com>
2025-07-10 17:24:21 +0900

    ✨feat: AladinApiService 인터페이스 메소드 수정
    책 목록 조회 방식에서 책 단건 조회로 변경

--- src/main/java/checkmo/domain/book/service/query/AladinApiService.java
+++ src/main/java/checkmo/domain/book/service/query/AladinApiService.java
@@ -2,14 +2,13 @@ package checkmo.domain.book.service.query;
 
 import checkmo.domain.book.dto.BookResponseDTO;
 
-import java.util.List;
-
 public interface AladinApiService {
     /**
      * 알라딘 API를 통해 책 정보를 가져오는 메서드
      *
      * @param keyword 책 제목 + 저자명
+     * @param page 페이지 번호
      * @return 책 정보 List DTO
      */
-    List<BookResponseDTO.BookInfoDetailResponseDTO> getBookInfoFromAladin(String keyword);
+    BookResponseDTO.BookListResponseDTO getBookInfoFromAladin(String keyword, int page);
 }
