c188aa10abc0b49d6ed88d524fe4a609c9c07806
MODUGGAGI <kiroro0814@naver.com>
2025-07-10 21:49:11 +0900

    ✨feat: MemberQueryService 메소드 추가
    회원 기본 정보, 프로필 조회 및 닉네임으로 회원 ID 조회 메소드 추가.

--- src/main/java/checkmo/domain/member/service/query/MemberQueryService.java
+++ src/main/java/checkmo/domain/member/service/query/MemberQueryService.java
@@ -1,9 +1,36 @@
 package checkmo.domain.member.service.query;
 
+import checkmo.domain.member.dto.MemberResponseDTO;
+
 /**
  * 회원 기본 정보 조회 서비스
  *
  * 회원과 관련된 모든 조회 서비스 여기서 처리
  */
 public interface MemberQueryService {
+
+    /**
+     * 회원 기본 정보 조회
+     *
+     * @param memberId 회원 ID
+     * @return 회원 기본 정보 DTO
+     */
+    MemberResponseDTO.MemberProfileResponseDTO getMemberBasicInfo(String memberId);
+
+    /**
+     * 다른 사람 프로필 조회
+     *
+     * @param targetMemberNickname 조회 대상 회원 닉네임
+     * @param memberId 조회하는 회원 ID (팔로우 여부 확인용)
+     * @return targetMember의 프로필 정보 DTO - 이때는 관심 카테고리 정보 DTO에 포함 X , -> 반드시 CategoryQueryFacade를 통해 조회해야 함
+     */
+    MemberResponseDTO.otherProfileResponseDTO getProfile(String targetMemberNickname, String memberId);
+
+    /**
+     * 닉네임으로 회원 ID 조회
+     *
+     * @param nickname 닉네임
+     * @return 회원 ID
+     */
+    String getMemberIdByNickname(String nickname);
 }
