993aaa0973bffeabaafd84eba17325b429b6cc2a
MODUGGAGI <kiroro0814@naver.com>
2025-07-11 22:20:06 +0900

    ✨feat: ClubBookRecommendCommandService 인터페이스 추가하여 책 추천 기능 구현

---
+++ src/main/java/checkmo/domain/club/service/command/ClubBookRecommendCommandService.java
@@ -0,0 +1,36 @@
+package checkmo.domain.club.service.command;
+
+import checkmo.domain.club.dto.club.ClubRequestDTO;
+
+public interface ClubBookRecommendCommandService {
+
+    /**
+     * 독서모임에 책을 추천합니다.
+     *
+     * @param clubId 독서모임 ID
+     * @param memberId 추천하는 회원 ID -> 클럽 회원인지 확인하는 로직 필요
+     * @param request 추천할 책 정보 DTO
+     * @return 추천한 책의 ID
+     */
+    Long recommendBook(Long clubId, String memberId, ClubRequestDTO.CreateBookRecommendDTO request);
+
+    /**
+     * 독서모임에 추천 책을 수정합니다.
+     *
+     * @param clubId 독서모임 ID
+     * @param memberId 추천하는 회원 ID -> 클럽 회원인지 확인하는 로직 필요
+     * @param bookRecommendId 수정할 추천 책의 ID
+     * @param request 수정할 추천책의 정보 DTO
+     * @return 수정한 추천 책의 ID
+     */
+    Long updateBookRecommend(Long clubId, String memberId, Long bookRecommendId, ClubRequestDTO.UpdateBookRecommendDTO request);
+
+    /**
+     * 독서모임에서 추천한 책을 삭제합니다.
+     *
+     * @param clubId 독서모임 ID
+     * @param memberId 삭제하는 회원 ID -> 클럽 회원인지 확인하는 로직 필요
+     * @param bookRecommendId 삭제할 추천 책의 ID
+     */
+    void deleteRecommendedBook(Long clubId, String memberId, Long bookRecommendId);
+}
