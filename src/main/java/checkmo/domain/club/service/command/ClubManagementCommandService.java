c1854249e327671cab1d3bf1118e0a561baaf9b5
MODUGGAGI <kiroro0814@naver.com>
2025-07-11 18:22:30 +0900

    ✨feat: ClubManagementCommandService에서 memberId 파라미터 타입을 Long에서 String으로 변경

--- src/main/java/checkmo/domain/club/service/command/ClubManagementCommandService.java
+++ src/main/java/checkmo/domain/club/service/command/ClubManagementCommandService.java
@@ -16,7 +16,7 @@ public interface ClubManagementCommandService {
      * @param request 모임 생성 요청 DTO
      * @return 생성된 독서모임 ID
      */
-    Long createClub(Long memberId, ClubRequestDTO.ClubDetailDTO request);
+    Long createClub(String memberId, ClubRequestDTO.ClubDetailDTO request);
 
     /**
      * 독서모임을 수정합니다.
@@ -24,9 +24,9 @@ public interface ClubManagementCommandService {
      * 피그마 참고 페이지 : # 아직 피그마 페이지 없음
      *
      * @param clubId 독서모임 ID
-     * @param memberId 운영진 ID -> 운영진인지 확인하는 로직 필요 ClubMember에서 Role 확인 -> 어노테이션으�� 처리 고려
+     * @param memberId 운영진 ID -> 운영진인지 확인하는 로직 필요 ClubMember에서 Role 확인 -> 어노테이션으로 처리 고려
      * @param request 모임 수정 요청 DTO
      * @return 생성된 독서모임 ID
      */
-    Long updateClub(Long clubId, Long memberId, ClubRequestDTO.ClubDetailDTO request);
+    Long updateClub(Long clubId, String memberId, ClubRequestDTO.ClubDetailDTO request);
 }
