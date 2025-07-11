a8117625640b10397a0f486c85a36012f46a865e
MODUGGAGI <kiroro0814@naver.com>
2025-07-11 15:59:34 +0900

    ✨feat: ClubQueryService에 클럽 조회 관련 메소드 추가

--- src/main/java/checkmo/domain/club/service/query/ClubQueryService.java
+++ src/main/java/checkmo/domain/club/service/query/ClubQueryService.java
@@ -1,10 +1,42 @@
 package checkmo.domain.club.service.query;
 
+import checkmo.domain.club.dto.club.ClubResponseDTO;
+
 /**
- * 독서 모임 조회 서비스
+ * 독서 클럽 조회 서비스
  *
- * 독서 모임 자체에 대한 조회 기능을 담당
- * ex) 모임 기본 정보 조회, 모임 목록 조회, 검색 기능, 특정 모임 상세 정보 조회 등을 처리
+ * 독서 클럽 자체에 대한 조회 기능을 담당
+ * ex) 독서 클럽 목록 조회, 검색 기능, 특정 독서 클럽 상세 정보 조회 등을 처리
  */
 public interface ClubQueryService {
+    /**
+     * 독서 클럽 목록을 조회합니다.
+     *
+     * 피그마 참고 페이지 : #독서모임 - 모임 검색하기
+     *
+     * @param keyword 검색 키워드 (모임명 등)
+     * @param region 지역 필터 (0: 지역 필터 선택 안함 / 1: 지역 필터 선택해서 검색 키워드로 지역명도 검색 가능)
+     * @param participants 지역 필터 (0: 동아리 대상별 검색 필터 선택 안함 / 1: 동아리 대상별 검색 필터 선택해서 검색 키워드로 동아리 대상도 검색 가능)
+     * @return 독서 클럽 목록 DTO
+     */
+    ClubResponseDTO.ClubListDTO getClubList(String keyword, int region, int participants);
+
+    /**
+     * 내가 가입한 독서 클럽 목록을 전체 조회합니다.
+     *
+     * 피그마 참고 페이지 : #독서모임 - 내 모임 바로가기
+     * @param memberId 회원 ID -> 로그인한 회원의 ID를 사용
+     * @return 내가 가입한 독서 클럽 목록 DTO
+     */
+    ClubResponseDTO.MyClubListDTO getMyClubList(String memberId);
+
+    /**
+     * 내가 가입한 독서 클럽 목록을 size 개수만큼 조회합니다.
+     *
+     * 피그마 참고 페이지 : #독서모임 - 내 모임 바로가기
+     * @param memberId 회원 ID -> 로그인한 회원의 ID를 사용
+     * @param size 조회할 개수
+     * @return 내가 가입한 독서 클럽 목록 DTO
+     */
+    ClubResponseDTO.MyClubListDTO getMyClubList(String memberId, int size);
 }
