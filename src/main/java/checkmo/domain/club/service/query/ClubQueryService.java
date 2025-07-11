a949d586f5eec2c19c2985c5054be689bcdd4aec
MODUGGAGI <kiroro0814@naver.com>
2025-07-11 18:21:59 +0900

    ✨feat: ClubQueryService에 공지사항 조회 메서드 및 커서 ID 파라미터 추가

--- src/main/java/checkmo/domain/club/service/query/ClubQueryService.java
+++ src/main/java/checkmo/domain/club/service/query/ClubQueryService.java
@@ -17,9 +17,10 @@ public interface ClubQueryService {
      * @param keyword 검색 키워드 (모임명 등)
      * @param region 지역 필터 (0: 지역 필터 선택 안함 / 1: 지역 필터 선택해서 검색 키워드로 지역명도 검색 가능)
      * @param participants 지역 필터 (0: 동아리 대상별 검색 필터 선택 안함 / 1: 동아리 대상별 검색 필터 선택해서 검색 키워드로 동아리 대상도 검색 가능)
+     * @param cursorId 커서 ID (페이징을 위한 커서, 처음에는 null 또는 0)
      * @return 독서 클럽 목록 DTO
      */
-    ClubResponseDTO.ClubListDTO getClubList(String keyword, int region, int participants);
+    ClubResponseDTO.ClubListDTO getClubList(String keyword, int region, int participants, Long cursorId);
 
     /**
      * 내가 가입한 독서 클럽 목록을 전체 조회합니다.
@@ -48,9 +49,10 @@ public interface ClubQueryService {
      * @param clubId 독서모임 ID
      * @param memberId 운영진 ID -> 운영진인지 확인하는 로직 필요 ClubMember에서 Role 확인 -> 어노테이션으로 처리 고려
      * @param clubMemberStatus 검색할 회원들의 상태 (예: "MEMBER", "STAFF", "PENDING", "BLOCKED")
+     * @param cursorId 커서 ID (페이징을 위한 커서, 처음에는 null 또는 0)
      * @return 검색한 독서모임 멤버 목록 DTO (승인 안된 상태의 독서 모임 멤버도 포함될 수 있음)
      */
-    ClubResponseDTO.ClubMemberListDTO getClubMemberListByStatus(String clubId, String memberId, String clubMemberStatus);
+    ClubResponseDTO.ClubMemberListDTO getClubMemberListByStatus(Long clubId, String memberId, String clubMemberStatus, Long cursorId);
 
     /**
      * 독서모임의 상세 정보를 조회합니다.
@@ -61,7 +63,7 @@ public interface ClubQueryService {
      * @param memberId 운영진 ID -> 운영진인지 확인하는 로직 필요 ClubMember에서 Role 확인 -> 어노테이션으로 처리 고려
      * @return 독서 클럽 상세 정보 DTO
      */
-    ClubResponseDTO.ClubDetailDTO getClubInfo(String clubId, String memberId);
+    ClubResponseDTO.ClubDetailDTO getClubInfo(Long clubId, String memberId);
 
     /**
      * 독서모임의 이름 중복 여부를 확인 합니다.
@@ -72,4 +74,27 @@ public interface ClubQueryService {
      * @return 중복 여부 (true: 중복, false: 중복 아님)
      */
     boolean isDuplicateClubName(String clubName);
+
+    /**
+     * 중요 공지사항 최신순 size 개수만큼 조회 (최상단에 들어갈 공지사항들)
+     *
+     * 피그마 참고 페이지 : #독서모임(운영진) - 공지사항 홈화면
+     *
+     * @param clubId 독서모임 ID
+     * @param memberId 회원의 ID   -> 해당 독서 클럽에만 포함되면 전부 조회 가능
+     * @param size 조회할 개수
+     * @return 중요 공지사항 목록 DTO
+     */
+    ClubResponseDTO.ClubNoticeListDTO getLatestNotices(Long clubId, String memberId, int size);
+
+    /**
+     * 공지사항 최신순으로 전체 조회
+     *
+     * 피그마 참고 페이지 : #독서모임(운영진) - 공지사항 홈화면
+     * @param clubId 독서모임 ID
+     * @param memberId 회원의 ID   -> 해당 독서 클럽에만 포함되면 전부 조회 가능
+     * @param cursorId 커서 ID (페이징을 위한 커서, 처음에는 null 또는 0)
+     * @return 전체 공지사항 목록 DTO
+     */
+    ClubResponseDTO.ClubNoticeListDTO getLatestNotices(Long clubId, String memberId, Long cursorId);
 }
