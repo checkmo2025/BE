322ff9801190580ea3f33ee402bbc32328c51b14
MODUGGAGI <kiroro0814@naver.com>
2025-07-11 16:28:24 +0900

    ✨feat: ClubResponseDTO 및 ClubRequestDTO에 클럽 관련 새로운 DTO 추가 및 필드 추가

--- src/main/java/checkmo/domain/club/dto/club/ClubRequestDTO.java
+++ src/main/java/checkmo/domain/club/dto/club/ClubRequestDTO.java
@@ -1,4 +1,13 @@
 package checkmo.domain.club.dto.club;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class ClubRequestDTO {
+
+    @Getter
+    @NoArgsConstructor
+    public static class ClubMemberJoinDTO {
+        private String joinMessage;
+    }
 }
--- src/main/java/checkmo/domain/club/dto/club/ClubResponseDTO.java
+++ src/main/java/checkmo/domain/club/dto/club/ClubResponseDTO.java
@@ -34,7 +34,8 @@ public class ClubResponseDTO {
     @Builder
     public static class ClubInfoDTO {
         private Long clubId;         // 모임 ID
-        private String clubName;     // 모임 이름
+        private String clubName;     // 모임 이름, joinClub의 반환값에서 사용될 때는 null
+        private Boolean isOpen;      // 모임 공개 여부 (true: 공개, false: 비공개), MyClubListDTO-ClubInfoDTO에서 사용될 때는 null
     }
 
     @Getter
@@ -48,9 +49,31 @@ public class ClubResponseDTO {
         private String description;  // 모임 소개
         private String participants; // 모임 대상
         private String category;     // 모임 카테고리
-        private String region;     // 모임 장소
+        private String region;       // 모임 장소
         private String insta;        // 모임 인스타그램 URL
         private String kakao;        // 모임 카카오톡 URL
+        private boolean isOpen;      // 모임 공개 여부 (true: 공개, false: 비공개)
     }
 
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class ClubMemberListDTO {
+        private List<ClubMemberDTO> clubMembers; // 모임 회원 목록
+        private boolean hasNext; // 다음 페이지 존재 여부
+        private Long nextCursor; // 다음 페이지 커서 (마지막 항목의 ID)
+        private int pageSize; // 현재 페이지 크기
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class ClubMemberDTO {
+        private String nickname; // 회원의 닉네임
+        private String profileImgUrl; // 회원의 프로필 이미지 URL
+        private String joinMessage; // 회원의 가입 메시지, ClubMemberStatus가 PENDING인 경우에만 사용됨
+        private String clubMemberStatus; // 회원의 상태 (예: "MEMBER", "STAFF", "PENDING", "BLOCKED")
+    }
 }
