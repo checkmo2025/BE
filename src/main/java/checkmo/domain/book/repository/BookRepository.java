1f56c9149d14d0d12abf50c4700fdc1acd04ab22
MODUGGAGI <kiroro0814@naver.com>
2025-07-14 15:51:15 +0900

    ♻️refactor: id 필드를 Long에서 String으로 변경

--- src/main/java/checkmo/domain/book/repository/BookRepository.java
+++ src/main/java/checkmo/domain/book/repository/BookRepository.java
@@ -3,5 +3,5 @@ package checkmo.domain.book.repository;
 import checkmo.domain.book.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, String> {
 }
--- src/main/java/checkmo/domain/member/repository/MemberRepository.java
+++ src/main/java/checkmo/domain/member/repository/MemberRepository.java
@@ -3,5 +3,5 @@ package checkmo.domain.member.repository;
 import checkmo.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, String> {
 }
