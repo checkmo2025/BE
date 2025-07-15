8a9eb7368b4b3b0df64dfa9e4745715bcb65680e
MODUGGAGI <kiroro0814@naver.com>
2025-07-15 20:32:25 +0900

    ✨feat: BookStory 등록을 위해서는 단순 bookId가 아닌 책 정보 전체가 있어야 하기 때문에 BookInfoDTO 추가

--- src/main/java/checkmo/domain/bookStory/dto/BookStoryRequestDTO.java
+++ src/main/java/checkmo/domain/bookStory/dto/BookStoryRequestDTO.java
@@ -8,9 +8,20 @@ public class BookStoryRequestDTO {
     @Getter
     @NoArgsConstructor
     public static class BookStoryCreateRequestDTO {
-        private String bookId;
+        private BookInfoDTO bookInfo; // 책 정보
         private String title;
         private String description;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class BookInfoDTO {
+        private String isbn; // ISBN 번호 (책의 고유 ID)
+        private String title; // 책 제목
+        private String author; // 저자
+        private String imgUrl; // 책 이미지 URL
+        private String publisher; // 출판사
+        private String description; // 책 설명
     }
 
     @Getter
