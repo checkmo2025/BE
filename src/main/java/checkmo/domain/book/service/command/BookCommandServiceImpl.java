cd741fd9af8249dcdbed6d8fed62db1c90a1d681
MODUGGAGI <kiroro0814@naver.com>
2025-07-17 15:59:35 +0900

    ✨feat: BookCommandServiceImpl에 책 중복 저장 방지 및 삭제 시 예외 처리 추가

--- src/main/java/checkmo/domain/book/service/command/BookCommandServiceImpl.java
+++ src/main/java/checkmo/domain/book/service/command/BookCommandServiceImpl.java
@@ -1,5 +1,7 @@
 package checkmo.domain.book.service.command;
 
+import checkmo.apiPayload.code.status.ErrorStatus;
+import checkmo.apiPayload.exception.GeneralException;
 import checkmo.domain.book.converter.BookConverter;
 import checkmo.domain.book.entity.Book;
 import checkmo.domain.book.repository.BookRepository;
@@ -19,12 +21,23 @@ public class BookCommandServiceImpl implements BookCommandService {
     @Transactional
     public void saveBook(BookSharedDTO.BookCreateRequestDTO request) {
         Book book = BookConverter.fromBookCreateRequestDTO(request);
+
+        // 이미 존재하는 책인지 확인
+        if (bookRepository.existsById(book.getId())) {
+            return; // 이미 존재하면 저장하지 않음
+        }
+
         bookRepository.save(book);
     }
 
     @Override
     @Transactional
     public void deleteBook(String bookId) {
-        bookRepository.deleteById(bookId);
+
+        Book book = bookRepository.findById(bookId).orElseThrow(
+                () -> new GeneralException(ErrorStatus.BOOK_NOT_FOUND)
+        );
+
+        bookRepository.delete(book);
     }
 }
