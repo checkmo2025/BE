d6dcb381b84d6bfda61ac42a0aaf426f5a27ed8c
MODUGGAGI <kiroro0814@naver.com>
2025-07-17 13:46:21 +0900

    ✨feat: BookConverter 클래스 추가해서 Aladin API 응답을 BookResponseDTO로 변환

---
+++ src/main/java/checkmo/domain/book/converter/BookConverter.java
@@ -0,0 +1,73 @@
+package checkmo.domain.book.converter;
+
+import checkmo.domain.book.web.dto.AladinApiResponseDTO;
+import checkmo.domain.book.web.dto.BookResponseDTO;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class BookConverter {
+
+    public static BookResponseDTO.BookInfoDetailResponseDTO fromAladinBookItem(
+            AladinApiResponseDTO.AladinBookItem item
+    ) {
+        return BookResponseDTO.BookInfoDetailResponseDTO.builder()
+                .isbn(item.getIsbn13())
+                .title(item.getTitle())
+                .author(item.getAuthor())
+                .imgUrl(item.getCover())
+                .publisher(item.getPublisher())
+                .description(item.getDescription())
+                .build();
+    }
+
+    public static BookResponseDTO.BookListResponseDTO fromAladinApiResponse(
+            AladinApiResponseDTO.AladinApiResponse aladinApiResponse,
+            int page
+    ) {
+        if (isInvalidResponse(aladinApiResponse)) {
+            return createEmptyBookListResponse();
+        }
+
+        List<BookResponseDTO.BookInfoDetailResponseDTO> books = convertToBookList(aladinApiResponse.getItems());
+        boolean hasNext = calculateHasNext(aladinApiResponse);
+
+        return BookResponseDTO.BookListResponseDTO.builder()
+                .bookInfoDetailResponseDTOs(books)
+                .hasNext(hasNext)
+                .currentPage(page)
+                .build();
+    }
+
+    private static boolean isInvalidResponse(AladinApiResponseDTO.AladinApiResponse aladinApiResponse) {
+        return aladinApiResponse == null || aladinApiResponse.getItems() == null;
+    }
+
+    private static BookResponseDTO.BookListResponseDTO createEmptyBookListResponse() {
+        return BookResponseDTO.BookListResponseDTO.builder()
+                .bookInfoDetailResponseDTOs(List.of())
+                .hasNext(false)
+                .currentPage(null)
+                .build();
+    }
+
+    private static List<BookResponseDTO.BookInfoDetailResponseDTO> convertToBookList(
+            List<AladinApiResponseDTO.AladinBookItem> items) {
+        return items.stream()
+                .map(BookConverter::fromAladinBookItem)
+                .toList();
+    }
+
+    private static boolean calculateHasNext(AladinApiResponseDTO.AladinApiResponse aladinApiResponse) {
+        int totalResults = aladinApiResponse.getTotalResults();
+        int startIndex = aladinApiResponse.getStartIndex();
+        int itemsPerPage = aladinApiResponse.getItemsPerPage();
+        int itemsInThisResponse = aladinApiResponse.getItems().size();
+
+        int totalItemsFetched = (startIndex - 1) * itemsPerPage + itemsInThisResponse;
+
+        return totalItemsFetched < totalResults;
+    }
+}
