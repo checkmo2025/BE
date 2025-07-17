f59536f3e927644e9a3a3c58144fecebee7a3a8a
MODUGGAGI <kiroro0814@naver.com>
2025-07-17 15:13:31 +0900

    ✨feat: BookQueryServiceImpl 클래스 추가 및 책 조회 기능 구현

---
+++ src/main/java/checkmo/domain/book/service/query/BookQueryServiceImpl.java
@@ -0,0 +1,27 @@
+package checkmo.domain.book.service.query;
+
+import checkmo.domain.book.converter.BookConverter;
+import checkmo.domain.book.entity.Book;
+import checkmo.domain.book.repository.BookRepository;
+import checkmo.domain.book.web.dto.BookResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BookQueryServiceImpl implements BookQueryService {
+
+    private final BookRepository bookRepository;
+
+    @Override
+    public BookResponseDTO.BookInfoDetailResponseDTO findBook(String bookId) {
+
+        Book book = bookRepository.findById(bookId).orElseThrow(
+                () -> new IllegalArgumentException("책을 찾을 수 없습니다. bookId: " + bookId)
+        );
+
+        return BookConverter.fromBook(book);
+    }
+}
