2a539d6a08a66ff186b77fc366817a88540429c1
MODUGGAGI <kiroro0814@naver.com>
2025-07-17 15:05:31 +0900

    ♻️refactor: BookController에서 불필요한 import 문 제거 및 정리

--- src/main/java/checkmo/domain/book/web/controller/BookController.java
+++ src/main/java/checkmo/domain/book/web/controller/BookController.java
@@ -6,10 +6,7 @@ import checkmo.domain.book.facade.BookCommandFacade;
 import checkmo.domain.book.facade.BookQueryFacade;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/books")
