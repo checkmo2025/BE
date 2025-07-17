f136567b34b0edc4ad94de7a8473f90cb1d51c3c
MODUGGAGI <kiroro0814@naver.com>
2025-07-17 15:21:21 +0900

    ♻️refactor: BookSharedDTO에서 coverImageUrl 필드를 imgUrl로 변경

--- src/main/java/checkmo/global/dto/BookSharedDTO.java
+++ src/main/java/checkmo/global/dto/BookSharedDTO.java
@@ -28,7 +28,7 @@ public class BookSharedDTO {
         private String bookId;          // ISBN을 책 Id로 사용
         private String title;           // 책 제목
         private String author;          // 저자명
-        private String coverImageUrl;   // 책 표지 이미지 URL
+        private String imgUrl;   // 책 표지 이미지 URL
     }
 
     @Getter
@@ -39,7 +39,7 @@ public class BookSharedDTO {
         private String bookId;
         private String title;
         private String author;
-        private String coverImageUrl;
+        private String imgUrl;
         private String publisher;       // 출판사
         private String description;     // 책 설명/줄거리
     }
