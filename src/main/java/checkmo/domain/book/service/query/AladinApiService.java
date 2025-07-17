bc14de764132e87ba71421c673cbc96011e64e62
MODUGGAGI <kiroro0814@naver.com>
2025-07-17 21:09:19 +0900

    ✨feat: AladinApiService에 상세 조회 메서드 추가

--- src/main/java/checkmo/domain/book/service/query/AladinApiService.java
+++ src/main/java/checkmo/domain/book/service/query/AladinApiService.java
@@ -3,12 +3,21 @@ package checkmo.domain.book.service.query;
 import checkmo.domain.book.web.dto.BookResponseDTO;
 
 public interface AladinApiService {
+
     /**
-     * 알라딘 API를 통해 책 정보를 가져오는 메서드
+     * 알라딘 API를 통해 책을 검색하는 메서드
      *
      * @param keyword 책 제목 + 저자명
      * @param page 페이지 번호
      * @return 책 정보 List DTO
      */
-    BookResponseDTO.BookListResponseDTO getBookInfoFromAladin(String keyword, int page);
+    BookResponseDTO.BookListResponseDTO searchBookFromAladin(String keyword, int page);
+
+    /**
+     * 알라딘 API를 통해 특정 책을 조회하는 메서드
+     *
+     * @param isbn 책의 13자리 ISBN
+     * @return 책 정보 DTO
+     */
+    BookResponseDTO.BookInfoDetailResponseDTO getBookDetailInfoFromAladin(String isbn);
 }
