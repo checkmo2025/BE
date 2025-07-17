b0541f4a3876669809461d8f939bdb0f85affba5
MODUGGAGI <kiroro0814@naver.com>
2025-07-17 15:04:17 +0900

    ✨feat: BookCommandFacade 인터페이스에서 BookRequestDTO를 BookSharedDTO로 변경
    Book 엔티티로 저장하는 경우는 단독으로 쓰이지 않고 무조건 외부 엔티티(BookStory, BookRecommend, Meeting 등등)에서 쓰이기 때문에 외부용으로 바꾸고 BookSharedDTO 사용해서 외부에서도 호출 가능하게 바꾸었다.

--- src/main/java/checkmo/domain/book/facade/BookCommandFacade.java
+++ src/main/java/checkmo/domain/book/facade/BookCommandFacade.java
@@ -1,6 +1,6 @@
 package checkmo.domain.book.facade;
 
-import checkmo.domain.book.web.dto.BookRequestDTO;
+import checkmo.global.dto.BookSharedDTO;
 
 /**
  * Book Domain Command Facade
@@ -9,11 +9,11 @@ import checkmo.domain.book.web.dto.BookRequestDTO;
 public interface BookCommandFacade {
 
     /**
-     * 알라딘 API로부터 얻은 책 정보를 저장합니다. (내부용)
+     * 알라딘 API로부터 얻은 책 정보를 저장합니다. (외부용)
      *
      * @param request 저장할 책 정보 DTO
      */
-    void saveBookFromAladin(BookRequestDTO.Aladin2BookDTO request); //
+    void saveBookFromAladin(BookSharedDTO.BookCreateRequestDTO request); //
 
     /**
      * 특정 책 정보를 삭제합니다. (내부용)
