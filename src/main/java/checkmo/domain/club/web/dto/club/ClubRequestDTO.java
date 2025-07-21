81e0fd3eea179ac83704937a67046d5b90c69331
zjhj0814 <zjhj0814@gmail.com>
2025-07-22 05:46:25 +0900

    ✨feat: 독서모임 생성 requestDTO 어노테이션 기반 검증
    - 독서모임 생성 DTO에 '태그' 정보가 누락되어 추가

--- src/main/java/checkmo/domain/club/web/dto/club/ClubRequestDTO.java
+++ src/main/java/checkmo/domain/club/web/dto/club/ClubRequestDTO.java
@@ -1,5 +1,6 @@
 package checkmo.domain.club.web.dto.club;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -82,11 +83,17 @@ public class ClubRequestDTO {
     @Getter
     @NoArgsConstructor
     public static class BookDetailDTO {
+        @NotBlank(message = "ISBN 번호는 null, 빈 문자열(\"\"), 공백 문자(\" \")까지도 모두 허용하지 않습니다.")
         private String isbn; // ISBN 번호
+        @NotBlank(message = "책 제목은 null, 빈 문자열(\"\"), 공백 문자(\" \")까지도 모두 허용하지 않습니다.")
         private String title; // 책 제목
+        @NotBlank(message = "저자는 null, 빈 문자열(\"\"), 공백 문자(\" \")까지도 모두 허용하지 않습니다.")
         private String author; // 저자
+        @NotBlank(message = "책 이미지 URL은 null, 빈 문자열(\"\"), 공백 문자(\" \")까지도 모두 허용하지 않습니다.")
         private String imgUrl; // 책 이미지 URL
+        @NotBlank(message = "출판사는 null, 빈 문자열(\"\"), 공백 문자(\" \")까지도 모두 허용하지 않습니다.")
         private String publisher; // 출판사
+        @NotBlank(message = "책 설명은 null, 빈 문자열(\"\"), 공백 문자(\" \")까지도 모두 허용하지 않습니다.")
         private String description; // 책 설명
     }
 
