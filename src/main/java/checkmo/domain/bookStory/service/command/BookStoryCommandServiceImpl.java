a0dc486c33186407f396d678d3bb0b3520708c80
MODUGGAGI <kiroro0814@naver.com>
2025-07-22 19:48:58 +0900

    ✨feat: 책이야기 생성 메소드 추가
    Member, Book 전부 getReferenceId()로 프록시 객체 가져와서 @ManyToOne에 매핑해서 BookStory 객체 생성

--- src/main/java/checkmo/domain/bookStory/service/command/BookStoryCommandServiceImpl.java
+++ src/main/java/checkmo/domain/bookStory/service/command/BookStoryCommandServiceImpl.java
@@ -1,6 +1,14 @@
 package checkmo.domain.bookStory.service.command;
 
+import checkmo.domain.book.entity.Book;
+import checkmo.domain.book.facade.BookCommandFacade;
+import checkmo.domain.book.facade.BookQueryFacade;
+import checkmo.domain.bookStory.converter.BookStoryConverter;
+import checkmo.domain.bookStory.entity.BookStory;
+import checkmo.domain.bookStory.repository.BookStoryRepository;
 import checkmo.domain.bookStory.web.dto.BookStoryRequestDTO;
+import checkmo.domain.member.entity.Member;
+import checkmo.domain.member.facade.MemberQueryFacade;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,17 +20,34 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class BookStoryCommandServiceImpl implements BookStoryCommandService {
 
+    private final BookStoryRepository bookStoryRepository;
+    private final MemberQueryFacade memberQueryFacade;
+    private final BookCommandFacade bookCommandFacade;
+    private final BookQueryFacade bookQueryFacade;
+
     @Override
+    @Transactional
     public Long createBookStory(String memberId, BookStoryRequestDTO.BookStoryCreateRequestDTO request) {
-        return 0L;
+
+        bookCommandFacade.saveBook(request.getBookInfo());
+        Book proxyBook = bookQueryFacade.findBookReferenceById(request.getBookInfo().getIsbn());
+
+        Member proxyMember = memberQueryFacade.findMemberReferenceById(memberId);
+
+        BookStory bookStory = BookStoryConverter.fromBookStoryRequestDTO(request, proxyMember, proxyBook);
+        BookStory savedBookStory = bookStoryRepository.save(bookStory);
+
+        return savedBookStory.getId();
     }
 
     @Override
+    @Transactional
     public Long updateBookStory(String memberId, BookStoryRequestDTO.BookStoryUpdateRequestDTO request) {
         return 0L;
     }
 
     @Override
+    @Transactional
     public Long deleteBookStory(String memberId, Long bookStoryId) {
         return 0L;
     }
