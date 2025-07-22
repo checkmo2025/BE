a7dbbaae10ed0969a4826daa9b54d5388a4e7055
MODUGGAGI <kiroro0814@naver.com>
2025-07-22 19:39:01 +0900

    ✨feat: Repository에 특정 사용자가 BookStory에 좋아요 눌렀는지 확인하는 메소드 추가

--- src/main/java/checkmo/domain/bookStory/repository/BookStoryLikedRepository.java
+++ src/main/java/checkmo/domain/bookStory/repository/BookStoryLikedRepository.java
@@ -4,4 +4,9 @@ import checkmo.domain.bookStory.entity.BookStoryLiked;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookStoryLikedRepository extends JpaRepository<BookStoryLiked, Long> {
+
+    /**
+     * 특정 사용자가 BookStory를 좋아요했는지 여부 확인
+     */
+    boolean existsByMemberIdAndBookStoryId(String memberId, Long bookStoryId);
 }
