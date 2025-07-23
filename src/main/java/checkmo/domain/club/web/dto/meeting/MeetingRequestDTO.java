8a197cea37430b1094dada89a4d09760955fc090
zjhj0814 <zjhj0814@gmail.com>
2025-07-23 14:38:43 +0900

    ♻️refactor: 미팅 생성 시 오류 메시지 간결하게 변경

--- src/main/java/checkmo/domain/club/web/dto/meeting/MeetingRequestDTO.java
+++ src/main/java/checkmo/domain/club/web/dto/meeting/MeetingRequestDTO.java
@@ -16,17 +16,17 @@ public class MeetingRequestDTO {
     @Getter
     @NoArgsConstructor
     public static class MeetingCreateRequestDTO {
-        @NotBlank(message = "독서모임 제목은 null, 빈 문자열(\"\"), 공백 문자(\" \")까지도 모두 허용하지 않습니다.")
+        @NotBlank(message = "독서모임 제목은 필수 입력입니다.")
         private String title; // 독서모임 제목
         @NotNull(message = "미팅 날짜, 시간은 null이 될 수 없습니다.")
         private LocalDateTime meetingTime; // 미팅 날짜, 시간
-        @NotBlank(message = "독서모임 장소는 null, 빈 문자열(\"\"), 공백 문자(\" \")까지도 모두 허용하지 않습니다.")
+        @NotBlank(message = "독서모임 장소는 필수 입력입니다.")
         private String location; // 모임 장소
-        @NotBlank(message = "독서모임 설명은 null, 빈 문자열(\"\"), 공백 문자(\" \")까지도 모두 허용하지 않습니다.")
+        @NotBlank(message = "독서모임 설명은 필수 입력입니다.")
         private String content;  // 모임 내용 설명 (공지사항 내용)
         @Min(value = 1, message = "기수는 1 이상의 정수여야 합니다.")
         private int generation;  // 기수
-        @NotBlank(message = "독서모임 태그는 null, 빈 문자열(\"\"), 공백 문자(\" \")까지도 모두 허용하지 않습니다.")
+        @NotBlank(message = "독서모임 태그는 필수 입력입니다.")
         private String tag; // 태그
         @Valid
         @NotNull(message = "책 정보는 null이 될 수 없습니다.")
@@ -36,17 +36,17 @@ public class MeetingRequestDTO {
     @Getter
     @NoArgsConstructor
     public static class MeetingUpdateRequestDTO {
-        @NotBlank(message = "독서모임 제목은 null, 빈 문자열(\"\"), 공백 문자(\" \")까지도 모두 허용하지 않습니다.")
+        @NotBlank(message = "독서모임 제목은 필수 입력입니다.")
         private String title; // 독서모임 제목
         @NotNull(message = "미팅 날짜, 시간은 null이 될 수 없습니다.")
         private LocalDateTime meetingTime; // 미팅 날짜, 시간
-        @NotBlank(message = "독서모임 장소는 null, 빈 문자열(\"\"), 공백 문자(\" \")까지도 모두 허용하지 않습니다.")
+        @NotBlank(message = "독서모임 장소는 필수 입력입니다.")
         private String location; // 모임 장소
-        @NotBlank(message = "독서모임 설명은 null, 빈 문자열(\"\"), 공백 문자(\" \")까지도 모두 허용하지 않습니다.")
+        @NotBlank(message = "독서모임 설명은 필수 입력입니다.")
         private String content;  // 모임 내용 설명 (공지사항 내용)
         @Min(value = 1, message = "기수는 1 이상의 정수여야 합니다.")
         private int generation;  // 기수
-        @NotBlank(message = "독서모임 태그는 null, 빈 문자열(\"\"), 공백 문자(\" \")까지도 모두 허용하지 않습니다.")
+        @NotBlank(message = "독서모임 태그는 필수 입력입니다.")
         private String tag; // 태그
     }
 
