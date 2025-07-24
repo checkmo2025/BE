222c8c08f3bab858ef5eacbb25dffc19a8b318cb
MODUGGAGI <kiroro0814@naver.com>
2025-07-24 22:00:23 +0900

    ✨feat: 책 이야기 상세 조회 메소드의 파라미터 수정 및 필요없어진 메소드 제거

--- src/main/java/checkmo/domain/bookStory/service/query/BookStoryQueryService.java
+++ src/main/java/checkmo/domain/bookStory/service/query/BookStoryQueryService.java
@@ -10,10 +10,11 @@ public interface BookStoryQueryService {
     /**
      * 책 이야기 조회
      *
+     * @param memberId 조회하는 회원의 ID
      * @param bookStoryId 조회할 책 이야기의 ID
      * @return 조회된 책 이야기의 DTO
      */
-    BookStoryResponseDTO.BookStoryResponse getBookStory(Long bookStoryId);
+    BookStoryResponseDTO.BookStoryResponse getBookStory(String memberId, Long bookStoryId);
 
     /**
      * 특정 회원의 책 이야기 목록을 커서 기반으로 조회합니다.
@@ -22,19 +23,10 @@ public interface BookStoryQueryService {
      * @param cursorId 페이징을 위한 커서 ID (처음에는 null)
      * @return 조회된 책 이야기 목록 DTO
      */
-    BookStoryResponseDTO.BookStoryListResponse getMyBookStories(String memberId, Long cursorId);
+    BookStoryResponseDTO.BookStoryListResponse getMyBookStories(String memberId, String targetMemberNickname, Long cursorId);
 
     /**
-     * 특정 회원의 책 이야기 목록을 size 개수만큼 조회합니다.
-     *
-     * @param memberId 조회할 회원의 ID
-     * @param size     조회할 개수
-     * @return 조회된 책 이야기 목록 DTO
-     */
-    BookStoryResponseDTO.BookStoryListResponse getMyBookStories(String memberId, int size);
-
-    /**
-     * scope에 따라 책 이야기 목록을 조회합니다.
+     * scope에 따라 책 이야기 목록을 커서 기반으로 조회합니다.
      *
      * @param memberId 조회하는 회원의 ID
      * @param scope    조회 범위 (ALL, MY, CLUB)
