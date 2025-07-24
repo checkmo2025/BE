060dac49cac29ae7f2f59acad3ca8a25a0f5060b
MODUGGAGI <kiroro0814@naver.com>
2025-07-24 21:59:15 +0900

    ✨feat: 책 이야기 상세 조회 메소드의 파라미터에 조회하는 회원의 ID 추가 및 필요없는 메소드 제거

--- src/main/java/checkmo/domain/bookStory/facade/BookStoryQueryFacade.java
+++ src/main/java/checkmo/domain/bookStory/facade/BookStoryQueryFacade.java
@@ -13,21 +13,22 @@ public interface BookStoryQueryFacade {
     /**
      * 특정 책 이야기의 상세 정보를 조회합니다. (내부용)
      *
+     * @param memberId 조회하는 회원의 ID
      * @param bookStoryId 조회할 책 이야기 ID
      * @return 조회된 책 이야기 상세 정보 DTO
      */
-    BookStoryResponseDTO.BookStoryResponse getBookStory(Long bookStoryId);
+    BookStoryResponseDTO.BookStoryResponse getBookStory(String memberId, Long bookStoryId);
 
     /**
      * 특정 회원의 책 이야기 목록을 커서 기반으로 조회합니다. (내부용)
      * 여기서 특정 회원은 나 일수도, 다른 회원일 수도 있습니다.
      *
      * @param memberId 조회하는 회원의 ID
-     * @param targetMemberId 조회할 회원의 ID
+     * @param targetMemberNickname 조회할 회원의 닉네임
      * @param cursorId 페이징을 위한 커서 ID (처음에는 null)
      * @return 조회된 책 이야기 목록 DTO
      */
-    BookStoryResponseDTO.BookStoryListResponse getMyBookStories(String memberId, String targetMemberId, Long cursorId);
+    BookStoryResponseDTO.BookStoryListResponse getMyBookStories(String memberId, String targetMemberNickname, Long cursorId);
 
     /**
      * scope에 따라 책 이야기 목록을 조회합니다. (내부용)
@@ -40,16 +41,4 @@ public interface BookStoryQueryFacade {
      * @return scope에 따른 책 이야기 목록 DTO
      */
     BookStoryResponseDTO.BookStoryListResponse getBookStoriesByScope(String memberId, BookStoryRequestDTO.BookStoryScope scope, Long clubId, Long cursorId);
-
-    /**
-     * 특정 회원의 책 이야기 미리보기 목록을 조회합니다. (외부용)
-     * 홈 화면, 마이페이지 등 다른 도메인/서비스에서 사용합니다.
-     * 여기서 특정 회원은 나 일수도, 다른 회원일 수도 있습니다.
-     *
-     * @param memberId 조회하는 회원의 ID
-     * @param targetMemberId 조회할 회원의 ID
-     * @param size     조회할 개수
-     * @return 책 이야기 미리보기 정보가 담긴 공유 DTO
-     */
-    BookStorySharedDTO.BookStoryPreviewListDTO getBookStoryPreviews(String memberId, String targetMemberId, int size);
 }
