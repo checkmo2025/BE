917830cc1a08212750c754398027b7c81cee5576
MODUGGAGI <kiroro0814@naver.com>
2025-07-24 22:00:05 +0900

    ✨feat: 책 이야기 상세 조회 시 bookStoryQueryService.getBookStory()로 연결 및 필요없는 메소드 제거

--- src/main/java/checkmo/domain/bookStory/facade/BookStoryQueryFacadeImpl.java
+++ src/main/java/checkmo/domain/bookStory/facade/BookStoryQueryFacadeImpl.java
@@ -3,7 +3,6 @@ package checkmo.domain.bookStory.facade;
 import checkmo.domain.bookStory.service.query.BookStoryQueryService;
 import checkmo.domain.bookStory.web.dto.BookStoryRequestDTO;
 import checkmo.domain.bookStory.web.dto.BookStoryResponseDTO;
-import checkmo.global.dto.BookStorySharedDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,22 +15,17 @@ public class BookStoryQueryFacadeImpl implements BookStoryQueryFacade {
     private final BookStoryQueryService bookStoryQueryService;
 
     @Override
-    public BookStoryResponseDTO.BookStoryResponse getBookStory(Long bookStoryId) {
-        return null;
+    public BookStoryResponseDTO.BookStoryResponse getBookStory(String memberId, Long bookStoryId) {
+        return bookStoryQueryService.getBookStory(memberId, bookStoryId);
     }
 
     @Override
-    public BookStoryResponseDTO.BookStoryListResponse getMyBookStories(String memberId, String targetMemberId, Long cursorId) {
+    public BookStoryResponseDTO.BookStoryListResponse getMyBookStories(String memberId, String targetMemberNickname, Long cursorId) {
         return null;
     }
 
     @Override
     public BookStoryResponseDTO.BookStoryListResponse getBookStoriesByScope(String memberId, BookStoryRequestDTO.BookStoryScope scope, Long clubId, Long cursorId) {
         return bookStoryQueryService.getBookStoriesByScope(memberId, scope, clubId, cursorId);
-    }
-
-    @Override
-    public BookStorySharedDTO.BookStoryPreviewListDTO getBookStoryPreviews(String memberId, String targetMemberId, int size) {
-        return null;
     }
 }
