c4caaf694fc494293833845a1cfc16ccc30acf6a
chaechaen <leece144@gmail.com>
2025-07-25 16:04:44 +0900

    ✨feat: Swagger 스키마 추가

--- src/main/java/checkmo/domain/member/web/dto/MemberRequestDTO.java
+++ src/main/java/checkmo/domain/member/web/dto/MemberRequestDTO.java
@@ -1,5 +1,6 @@
 package checkmo.domain.member.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -42,14 +43,18 @@ public class MemberRequestDTO {
         private String verificationCode;
     }
 
+    @Getter
+    @NoArgsConstructor
     public static class SignUpRequestDTO {
         @NotBlank(message = "이메일은 필수입니다")
         @Email(message = "유효한 이메일 형식이 아닙니다")
+        @Schema(description = "이메일 주소", example = "test@example.com")
         private String email;
 
         @NotBlank(message = "비밀번호는 필수입니다")
         @Size(min = 6, max = 10, message = "비밀번호는 6-10자여야 합니다")
         @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^&*]).*$", message = "비밀번호는 영어 및 특수문자를 포함해야 합니다")
+        @Schema(description = "비밀번호(영어+특수문자 포함 6~10자", example = "pass123!")
         private String password;
     }
 
