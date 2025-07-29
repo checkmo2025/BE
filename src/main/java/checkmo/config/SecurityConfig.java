2afcd55c88be5ab3ce52c3a8b765e6dea405bec5
chaechaen <leece144@gmail.com>
2025-07-29 18:53:43 +0900

    ♻️refactor: 프로필 미완료 필터 적용 및 경로 접근 제어 수정

--- src/main/java/checkmo/config/SecurityConfig.java
+++ src/main/java/checkmo/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package checkmo.config;
 
+import checkmo.domain.member.service.security.auth.ProfileCompletionAuthorizationFilter;
 import checkmo.domain.member.service.security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -23,20 +24,26 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http,
+                                           ProfileCompletionAuthorizationFilter profileCompletionAuthorizationFilter)
+        throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
             .httpBasic(AbstractHttpConfigurer::disable) // HTTP Basic 인증 비활성화
             .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 안함
-                )
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 안함
+            )
             .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll() // Swagger UI 접근 허용
-                        .requestMatchers("/**").permitAll() // TODO: 로그인 구현 후 삭제 필수
-                    .requestMatchers("/api/auth/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // JWT 인증 필터 추가
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**")
+                .permitAll() // Swagger UI 접근 허용
+                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/api/auth/additional-info").authenticated()
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(jwtAuthenticationFilter,
+                UsernamePasswordAuthenticationFilter.class) // JWT 인증 필터 추가
+            .addFilterAfter(profileCompletionAuthorizationFilter,
+                JwtAuthenticationFilter.class); // 프로필 완료 필터 추가
 
         return http.build();
     }
@@ -47,7 +54,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 }
