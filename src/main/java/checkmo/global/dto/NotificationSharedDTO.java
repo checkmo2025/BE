860cb2629f92361bde2599ea4aeb937d399fc582
MODUGGAGI <kiroro0814@naver.com>
2025-07-31 00:32:23 +0900

    ♻️refactor: 알림 미리보기 DTO 이름 변경 및 타입 수정

--- src/main/java/checkmo/global/dto/NotificationSharedDTO.java
+++ src/main/java/checkmo/global/dto/NotificationSharedDTO.java
@@ -1,5 +1,6 @@
 package checkmo.global.dto;
 
+import checkmo.domain.notification.entity.Notification;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,8 +23,8 @@ public class NotificationSharedDTO {
     @NoArgsConstructor
     @AllArgsConstructor
     @Builder
-    public static class NotificationPreviewListDTO {
-        private List<NotificationPreviewDTO> notifications;
+    public static class NotificationPreviewList {
+        private List<NotificationPreview> notifications;
     }
 
     /**
@@ -36,10 +37,10 @@ public class NotificationSharedDTO {
     @NoArgsConstructor
     @AllArgsConstructor
     @Builder
-    public static class NotificationPreviewDTO {
+    public static class NotificationPreview {
         private Long notificationId;
+        private Notification.NotificationType notificationType; // 알림 타입
         private String senderNickname;
-        private String receiverNickname;
         private boolean isRead;
         private LocalDateTime createdAt;
         private String redirectPath; // 알림 클릭 시 이동할 URL
