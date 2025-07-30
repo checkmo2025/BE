aa1d592355dc542aede0c1eb7e76da8e96ec342c
MODUGGAGI <kiroro0814@naver.com>
2025-07-30 18:48:43 +0900

    ✨feat: NotificationConverter에서 Event를 Notification으로 변환하는 메소드 추가 및 리다이렉트 경로 반환 메소드 구현

--- src/main/java/checkmo/domain/notification/converter/NotificationConverter.java
+++ src/main/java/checkmo/domain/notification/converter/NotificationConverter.java
@@ -1,8 +1,48 @@
 package checkmo.domain.notification.converter;
 
+import checkmo.domain.member.entity.Member;
+import checkmo.domain.notification.entity.Notification;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class NotificationConverter {
+
+    // =====================================================
+    // Event → Notification 변환
+    // =====================================================
+
+    /**
+     * Event -> Notification 변환
+     */
+    public static Notification fromEvent(
+            Notification.NotificationType notificationType,
+            String redirectPath,
+            Member proxySender,
+            Member proxyReceiver
+    ) {
+        return Notification.builder()
+                .notificationType(notificationType)
+                .redirectPath(redirectPath)
+                .sender(proxySender)
+                .receiver(proxyReceiver)
+                .build();
+    }
+
+    /**
+     * 리다이렉트 경로 반환
+     */
+    public static String getRedirectPath(Notification.NotificationType notificationType, Long bookStoryId) {
+        if (Notification.NotificationType.LIKE == notificationType) {
+            return "api/book-stories/" + bookStoryId;
+        }
+        return null; // 지금은 LIKE 타입일 경우 무조건 bookStoryId를 사용하지만, 다른 타입이 추가될 경우를 대비하여 null 반환
+    }
+
+    public static String getRedirectPath(Notification.NotificationType notificationType, String Nickname) {
+        if (Notification.NotificationType.FOLLOW == notificationType) {
+            return "/api/members/" + Nickname;
+        }
+        return null; // 지금은 FOLLOW 타입일 경우 무조건 Nickname을 사용하지만, 다른 타입이 추가될 경우를 대비하여 null 반환
+    }
 }
