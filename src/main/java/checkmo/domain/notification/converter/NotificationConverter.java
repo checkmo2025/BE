7d59f693cb1a7eeaa423f45b6cbbc1781e2f8503
MODUGGAGI <kiroro0814@naver.com>
2025-07-31 00:27:51 +0900

    ✨feat: 알림 변환 로직 추가 및 NotificationSharedDTO 변환 메소드 구현

--- src/main/java/checkmo/domain/notification/converter/NotificationConverter.java
+++ src/main/java/checkmo/domain/notification/converter/NotificationConverter.java
@@ -2,8 +2,12 @@ package checkmo.domain.notification.converter;
 
 import checkmo.domain.member.entity.Member;
 import checkmo.domain.notification.entity.Notification;
+import checkmo.global.dto.NotificationSharedDTO;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class NotificationConverter {
@@ -44,5 +48,43 @@ public class NotificationConverter {
             return "/api/members/" + Nickname;
         }
         return null; // 지금은 FOLLOW 타입일 경우 무조건 Nickname을 사용하지만, 다른 타입이 추가될 경우를 대비하여 null 반환
+    }
+
+    // =====================================================
+    // Notification → NotificationSharedDTO 변환
+    // =====================================================
+
+    /**
+     * NotificationPreviewDTO -> NotificationPreviewListDTO
+     */
+    public static NotificationSharedDTO.NotificationPreviewList convertToPreviewListDTO(
+            List<Notification> notifications,
+            Map<String, String> senderNicknameMap
+    ) {
+
+        List<NotificationSharedDTO.NotificationPreview> previewList = notifications.stream()
+                .map(notification -> convertToPreviewDTO(
+                        notification,
+                        senderNicknameMap.get(notification.getSenderId())
+                ))
+                .toList();
+
+        return NotificationSharedDTO.NotificationPreviewList.builder()
+                .notifications(previewList)
+                .build();
+    }
+
+    /**
+     * Notification → NotificationSharedDTO 변환
+     */
+    public static NotificationSharedDTO.NotificationPreview convertToPreviewDTO(Notification notification, String senderNickname) {
+        return NotificationSharedDTO.NotificationPreview.builder()
+                .notificationId(notification.getId())
+                .notificationType(notification.getNotificationType())
+                .senderNickname(senderNickname)
+                .isRead(notification.isRead())
+                .createdAt(notification.getCreatedAt())
+                .redirectPath(notification.getRedirectPath())
+                .build();
     }
 }
