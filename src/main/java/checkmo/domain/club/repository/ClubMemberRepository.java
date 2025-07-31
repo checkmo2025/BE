9a27b67432400198dd2fd072679fcc1908aac005
MODUGGAGI <kiroro0814@naver.com>
2025-07-31 22:55:58 +0900

    ✨feat: JPQL로 조회 후 바로 DTO로 변환하는 메소드 추가

--- src/main/java/checkmo/domain/club/repository/ClubMemberRepository.java
+++ src/main/java/checkmo/domain/club/repository/ClubMemberRepository.java
@@ -1,10 +1,18 @@
 package checkmo.domain.club.repository;
 
 import checkmo.domain.club.entity.ClubMember;
+import checkmo.global.dto.ClubSharedDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ClubMemberRepository extends JpaRepository<ClubMember, Long> {
     Optional<ClubMember> findByClubIdAndMemberId(Long clubId, String memberId);
+
+    @Query("SELECT new checkmo.global.dto.ClubSharedDTO.MyClubInfo(c.id, c.name) " +
+            "FROM ClubMember cm JOIN cm.club c WHERE cm.memberId = :memberId")
+    List<ClubSharedDTO.MyClubInfo> findMyClubInfoByMemberId(@Param("memberId") String memberId);
 }
