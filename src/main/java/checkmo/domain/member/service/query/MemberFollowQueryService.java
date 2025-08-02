8f5e563822df069da0c3c0f049d67761815c1e2d
MODUGGAGI <kiroro0814@naver.com>
2025-08-02 20:05:26 +0900

    ♻️refactor: FollowingList, FollowerList 전부 내부 내용이 동일해서 공통되게 FollowList로 수정 및 반환 형식을 DTO 반환에서 순수 객체 반환으로 변경

--- src/main/java/checkmo/domain/member/service/query/MemberFollowQueryService.java
+++ src/main/java/checkmo/domain/member/service/query/MemberFollowQueryService.java
@@ -1,6 +1,9 @@
 package checkmo.domain.member.service.query;
 
+import checkmo.domain.member.entity.Follow;
 import checkmo.domain.member.web.dto.MemberResponseDTO;
+
+import java.util.List;
 
 /**
  * 팔로우/팔로잉 조회 서비스
@@ -16,7 +19,7 @@ public interface MemberFollowQueryService {
      * @param cursorId 커서 ID (페이징을 위한) => 커서로 사용되는 ID는 Follow 엔티티 자체의 ID 값으로 사용하기!!
      * @return 팔로워 목록
      */
-     MemberResponseDTO.FollowerListResponseDTO getFollowers(String memberId, Long cursorId);
+    List<Follow> getFollowerList(String memberId, Long cursorId, int pageSize);
 
     /**
      * 특정 회원의 팔로잉 목록 전체 조회
@@ -25,7 +28,7 @@ public interface MemberFollowQueryService {
      * @param cursorId 커서 ID (페이징을 위한) => 커서로 사용되는 ID는 Follow 엔티티 자체의 ID 값으로 사용하기!!
      * @return 팔로잉 목록
      */
-    MemberResponseDTO.FollowingListResponseDTO getFollowing(String memberId, Long cursorId);
+    List<Follow> getFollowingList(String memberId, Long cursorId, int pageSize);
 
     /**
      * 특정 회원의 팔로우 목록 size 개수만큼 조회
@@ -33,7 +36,7 @@ public interface MemberFollowQueryService {
      * @param memberId 조회할 회원의 ID
      * @return 팔로워 목록
      */
-    MemberResponseDTO.FollowerListResponseDTO getFollowers(Long memberId, int size);
+    MemberResponseDTO.FollowList getFollowers(Long memberId, int size);
 
     /**
      * 특정 회원의 팔로잉 목록 size 개수만큼 조회
@@ -41,7 +44,7 @@ public interface MemberFollowQueryService {
      * @param memberId 조회할 회원의 ID
      * @return 팔로잉 목록
      */
-    MemberResponseDTO.FollowingListResponseDTO getFollowing(Long memberId, int size);
+    MemberResponseDTO.FollowList getFollowings(Long memberId, int size);
 
     /**
      * 특정 회원의 팔로우 여부 확인
