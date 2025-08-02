93788e39f8941e698d9fa3a8ae4ded592e232251
MODUGGAGI <kiroro0814@naver.com>
2025-08-02 19:59:50 +0900

    ✨feat: 커서 기반 페이징을 위한 조회 메소드 추가

--- src/main/java/checkmo/domain/member/repository/FollowRepository.java
+++ src/main/java/checkmo/domain/member/repository/FollowRepository.java
@@ -1,8 +1,11 @@
 package checkmo.domain.member.repository;
 
 import checkmo.domain.member.entity.Follow;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+
+import java.util.List;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
@@ -10,4 +13,14 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
 
     @Modifying
     void deleteByFollowerIdAndFollowingId(String followerId, String followingId);
+
+    List<Follow> findByFollowingIdOrderByIdDesc(String memberId, Pageable pageable);
+
+    List<Follow> findByFollowerIdOrderByIdDesc(String memberId, Pageable pageable);
+
+    List<Follow> findByFollowingIdAndIdLessThanOrderByIdDesc(String memberId, Long cursorId, Pageable pageable);
+
+    List<Follow> findByFollowerIdAndIdLessThanOrderByIdDesc(String memberId, Long cursorId, Pageable pageable);
+
+
 }
