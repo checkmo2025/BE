26d1086a977494e85a2d9f7e4724574a0c3effba
MODUGGAGI <kiroro0814@naver.com>
2025-08-02 16:56:23 +0900

    ✨feat: 팔로우 관계 삭제 메소드 추가

--- src/main/java/checkmo/domain/member/repository/FollowRepository.java
+++ src/main/java/checkmo/domain/member/repository/FollowRepository.java
@@ -2,8 +2,12 @@ package checkmo.domain.member.repository;
 
 import checkmo.domain.member.entity.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
     boolean existsByFollowerIdAndFollowingId(String followerId, String followingId);
+
+    @Modifying
+    void deleteByFollowerIdAndFollowingId(String followerId, String followingId);
 }
