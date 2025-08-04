d19fe5bd26f58fc61e5a2392f8094376946c4737
chaechaen <leece144@gmail.com>
2025-08-04 16:58:01 +0900

    ♻️refactor: 리다이렉트 경로에서 쿼리스트링 제외

--- src/main/java/checkmo/domain/member/service/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/checkmo/domain/member/service/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -1,6 +1,5 @@
 package checkmo.domain.member.service.security.oauth2;
 
-import checkmo.domain.member.service.security.auth.PrincipalDetails;
 import checkmo.domain.member.service.security.jwt.JwtLoginProcessor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -29,17 +28,8 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         // JWT 토큰 생성 및 쿠키 설정
         jwtLoginProcessor.processLogin(response, authentication);
 
-        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
-        boolean isProfileCompleted = principalDetails.getMember().isProfileCompleted();
-
         // 기본 리다이렉트 URI
         String redirectUri = "/api/auth/redirect/oauth2";
-
-        if (isProfileCompleted) {
-            redirectUri += "?nickname=" + principalDetails.getMember().getNickName();
-        } else {
-            redirectUri += "?isProfileCompleted=false&email=" + principalDetails.getMember().getEmail();
-        }
 
         // 성공 후 리다이렉트 URL 설정
         getRedirectStrategy().sendRedirect(request, response, redirectUri);
