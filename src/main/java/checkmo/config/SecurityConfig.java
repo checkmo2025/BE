e377fa90aff9938a7f6acd15eea4a2885472b463
MODUGGAGI <kiroro0814@naver.com>
2025-08-04 19:23:53 +0900

    ♻️refactor: 헬스체크는 아예 인증 적용하지 않기

--- src/main/java/checkmo/config/SecurityConfig.java
+++ src/main/java/checkmo/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -31,6 +32,12 @@ public class SecurityConfig {
     private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
 
     @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // /health 경로는 Spring Security 필터 체인을 아예 거치지 않도록 설정
+        return (web) -> web.ignoring().requestMatchers("/health");
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http)
         throws Exception {
         http
@@ -40,7 +47,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 안함
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/health", "/v3/api-docs/**", "/swagger-ui/**").permitAll() // Swagger UI 접근 허용
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll() // Swagger UI 접근 허용
                         .requestMatchers("/login/oauth2").permitAll() // OAuth2 로그인 허용
                         .requestMatchers("/api/auth/additional-info").authenticated()
                         .requestMatchers("/api/auth/**").permitAll()
