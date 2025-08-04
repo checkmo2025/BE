2f62d73fc5583239135e6ce462bdd2a8e8a7d5d2
MODUGGAGI <kiroro0814@naver.com>
2025-08-04 12:04:15 +0900

    ♻️refactor: ECR 로그인 명령 수정

--- scripts/codedeploy/after_install.sh
+++ scripts/codedeploy/after_install.sh
@@ -8,7 +8,7 @@ chmod +x ./scripts/codedeploy/*.sh
 
 # 2. AWS ECR 로그인
 echo "AWS ECR에 로그인"
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 268456953414.dkr.ecr.ap-northeast-2.amazonaws.com
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 667183278509.dkr.ecr.ap-northeast-2.amazonaws.com
 
 # 3. 최신 Docker 이미지 다운로드 (pull)
 echo "최신 Docker 이미지를 다운로드"
