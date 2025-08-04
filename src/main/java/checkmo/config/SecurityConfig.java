4a654d35f97ece37715b2639db482663298b81c1
chaechaen <leece144@gmail.com>
2025-08-05 01:25:49 +0900

    ♻️refactor: 홈 접근 허용

--- src/main/java/checkmo/config/SecurityConfig.java
+++ src/main/java/checkmo/config/SecurityConfig.java
@@ -40,8 +40,9 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 안함
                 )
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll() // 홈페이지 접근 허용
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "health").permitAll() // Swagger UI 접근 허용
-                        .requestMatchers("/login/oauth2").permitAll() // OAuth2 로그인 허용
+                        .requestMatchers("/login/oauth2/**").permitAll() // OAuth2 로그인 허용
                         .requestMatchers("/api/auth/additional-info").authenticated()
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
