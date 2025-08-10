96d6946063c5b8f76b89cdaedee1d746c7c0f8bf
chaechaen <leece144@gmail.com>
2025-08-11 02:19:36 +0900

    ♻️refactor: 소셜 로그인 성공 시 리다이렉트 URI를 프론트쪽으로 변경

--- src/main/java/checkmo/domain/member/service/security/jwt/JwtCookieUtil.java
+++ src/main/java/checkmo/domain/member/service/security/jwt/JwtCookieUtil.java
@@ -11,7 +11,7 @@ public class JwtCookieUtil {
     public void addTokenToCookie(HttpServletResponse response, String cookieName, String token, int maxAge) {
         Cookie cookie = new Cookie(cookieName, token);
         cookie.setHttpOnly(true); // 클라이언트 스크립트에서 접근 불가
-        cookie.setAttribute("SameSite", "Lax");
+        cookie.setAttribute("SameSite", "None");
         cookie.setPath("/"); // 모든 경로에서 접근 가능
         cookie.setMaxAge(maxAge);
         cookie.setSecure(true); // HTTPS에서만 전송하도록 추가
@@ -36,7 +36,7 @@ public class JwtCookieUtil {
         cookie.setMaxAge(0);
         cookie.setPath("/");
         cookie.setHttpOnly(true);
-        cookie.setAttribute("SameSite", "Lax");
+        cookie.setAttribute("SameSite", "None");
         response.addCookie(cookie);
     }
 }
--- src/main/java/checkmo/domain/member/service/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/checkmo/domain/member/service/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -1,13 +1,17 @@
 package checkmo.domain.member.service.security.oauth2;
 
+import checkmo.domain.member.entity.Member;
+import checkmo.domain.member.service.security.auth.PrincipalDetails;
 import checkmo.domain.member.service.security.jwt.JwtLoginProcessor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 /**
  * 소셜 로그인 성공 후의 성공 처리 핸들러
@@ -22,16 +26,26 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 
     private final JwtLoginProcessor jwtLoginProcessor;
 
+    @Value("${app.oauth2.redirect.base-uri}")
+    private String baseUri;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
 
         // JWT 토큰 생성 및 쿠키 설정
         jwtLoginProcessor.processLogin(response, authentication);
 
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+        Member member = principalDetails.getMember();
+
+        String path = member.isProfileCompleted() ? "/home" : "/profile";
+
         // 기본 리다이렉트 URI
-        String redirectUri = "/api/auth/redirect/oauth2";
+        String targetUrl = UriComponentsBuilder.fromUriString(baseUri)
+                                               .path(path)
+                                               .build().toUriString();
 
         // 성공 후 리다이렉트 URL 설정
-        getRedirectStrategy().sendRedirect(request, response, redirectUri);
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
 }
