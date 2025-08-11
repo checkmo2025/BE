dcf88bd3089d3ba62cb6414b1f175e5bc1fe331f
MODUGGAGI <kiroro0814@naver.com>
2025-08-11 15:02:23 +0900

    ✨feat: 이제 Member 내부의 S3Service를 사용하는 것이 아닌, 외부의 S3Service를 이용

--- src/main/java/checkmo/domain/member/service/command/MemberProfileCommandServiceImpl.java
+++ src/main/java/checkmo/domain/member/service/command/MemberProfileCommandServiceImpl.java
@@ -11,6 +11,7 @@ import checkmo.domain.member.web.dto.MemberRequestDTO;
 import checkmo.domain.member.web.dto.MemberResponseDTO;
 import checkmo.global.s3.service.S3Service;
 import checkmo.global.dto.CategorySharedDTO;
+import checkmo.global.s3.service.S3Service;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
