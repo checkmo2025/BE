6ade91e7453f1fb668ec40bc529362c9ddfe0f39
MODUGGAGI <kiroro0814@naver.com>
2025-08-12 01:00:41 +0900

    ✨feat: 클럽의 ID 타입에 맞게 Long으로 수정

--- src/main/java/checkmo/event/JoinClubEvent.java
+++ src/main/java/checkmo/event/JoinClubEvent.java
@@ -7,6 +7,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class JoinClubEvent {
     private final String memberId; // 클럽에 가입한 회원의 ID
-    private final String clubId;    // 가입한 클럽의 ID
+    private final Long clubId;    // 가입한 클럽의 ID
     private final String clubName;  // 가입한 클럽의 이름
 }
