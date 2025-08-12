528e797e7319130a2aae394bdd27528619bd4b0d
zjhj0814 <zjhj0814@gmail.com>
2025-08-12 12:52:37 +0900

    ♻️refactor: 미팅의 양방향 연관관계 정리
    - 미팅이 삭제되면 team이 삭제되는 정도의 영속성 설정이 필요
    - 연결됐던 팀이 고아객체가 되면 삭제 쿼리가 발생
    - team 양방향 연관관계 편의 메서드 추가

--- src/main/java/checkmo/domain/club/entity/meeting/Meeting.java
+++ src/main/java/checkmo/domain/club/entity/meeting/Meeting.java
@@ -60,7 +60,7 @@ public class Meeting extends BaseEntity {
     private Book book; // null 허용
 
     @Builder.Default
-    @OneToMany(mappedBy = "meeting", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "meeting", cascade = CascadeType.REMOVE, orphanRemoval = true)
     private List<Team> teams = new ArrayList<>();
 
     @OneToOne(mappedBy = "meeting", cascade = CascadeType.ALL, orphanRemoval = true)
@@ -130,5 +130,17 @@ public class Meeting extends BaseEntity {
     public void addTopic(Topic topic) {
         this.topics.add(topic);
         topic.setMeeting(this); // 주인 쪽에도 세팅
+    }
+
+    public void addTeam(Team team) {
+        if (team == null) return;
+        this.teams.add(team);
+        team.setMeeting(this);
+    }
+
+    public void removeTeam(Team team) {
+        if (team == null) return;
+        this.teams.remove(team);
+        team.setMeeting(null);
     }
 }
