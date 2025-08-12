113fc2aa6a3011345b1331d86950bb80db7f14b3
zjhj0814 <zjhj0814@gmail.com>
2025-08-12 12:46:09 +0900

    ✨feat: 미팅 ID로 팀 목록 조회 기능 추가

--- src/main/java/checkmo/domain/club/repository/meeting/TeamRepository.java
+++ src/main/java/checkmo/domain/club/repository/meeting/TeamRepository.java
@@ -3,5 +3,8 @@ package checkmo.domain.club.repository.meeting;
 import checkmo.domain.club.entity.meeting.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    List<Team> findTeamsByMeetingId(Long meetingId);
 }
