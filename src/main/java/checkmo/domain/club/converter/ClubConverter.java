4ba1021ea72b8e74c2156065b5e36019abede6ca
yoon <sin020511@gmail.com>
2025-08-13 12:59:10 +0900

    ♻️refactor: 모임 공지에 club 추가

--- src/main/java/checkmo/domain/club/converter/ClubConverter.java
+++ src/main/java/checkmo/domain/club/converter/ClubConverter.java
@@ -219,9 +219,6 @@ public class ClubConverter {
      */
     public static ClubResponseDTO.ClubNoticeWithClubDTO toClubNoticeWithClubDTO(Notice notice, ClubResponseDTO.NoticeItem noticeItemDTO) {
         var club = notice.getClub();
-        if (club == null && notice.getMeeting() != null) {
-            club = notice.getMeeting().getClub();
-        }
         if (club == null) {
             // 클럽 정보가 아예 없을 경우 null 처리
             return ClubResponseDTO.ClubNoticeWithClubDTO.builder()
@@ -660,6 +657,7 @@ public class ClubConverter {
                 .content(meeting.getContent())
                 .important(true)
                 .tag("모임")
+                .club(meeting.getClub())
                 .build();
     }
 
--- src/main/java/checkmo/domain/club/service/command/ClubMeetingCommandServiceImpl.java
+++ src/main/java/checkmo/domain/club/service/command/ClubMeetingCommandServiceImpl.java
@@ -58,11 +58,11 @@ public class ClubMeetingCommandServiceImpl implements ClubMeetingCommandService 
         // 3. 미팅 생성 후 proxyBook 연결
         Meeting meeting = ClubConverter.fromMeetingCreateRequestDTOToMeeting(request, proxyBook);
 
-        // 4. 공지 생성
-        Notice notice = ClubConverter.fromMeetingToNotice(meeting);
-
         // 5. 연관관계 설정
         club.addMeeting(meeting); //영속성 컨텍스트 내 객체 상태 동기화
+
+        // 6. 공지 생성
+        Notice notice = ClubConverter.fromMeetingToNotice(meeting);
         meeting.addNotice(notice);
 
         // 6. 미팅 명시적 저장 -> 공지사항도 함께 저장됨
