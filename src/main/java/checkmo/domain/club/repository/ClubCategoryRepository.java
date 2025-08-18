0a2c7a6174711b00f54931c01bd7498e691f133d
MODUGGAGI <kiroro0814@naver.com>
2025-08-19 01:18:12 +0900

    ✨ feat: ClubCategoryRepository 추가

---
+++ src/main/java/checkmo/domain/club/repository/ClubCategoryRepository.java
@@ -0,0 +1,14 @@
+package checkmo.domain.club.repository;
+
+import checkmo.domain.club.entity.ClubCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ClubCategoryRepository extends JpaRepository<ClubCategory, Long> {
+    // 클럽 ID로 ClubCategory 리스트 조회
+    List<ClubCategory> findByClubId(Long clubId);
+
+    List<ClubCategory> findByClubIdIn(List<Long> clubIds);
+
+}
