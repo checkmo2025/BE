2b6f3dbcad1dc3435e02981dba7b09037974f05b
MODUGGAGI <kiroro0814@naver.com>
2025-08-19 14:07:16 +0900

    ♻️ refactor: 회원의 카테고리 조회를 처리하는 MemberCategoryQueryService 생성

---
+++ src/main/java/checkmo/domain/member/service/query/MemberCategoryQueryService.java
@@ -0,0 +1,18 @@
+package checkmo.domain.member.service.query;
+
+import checkmo.domain.member.entity.MemberCategory;
+
+import java.util.List;
+
+/**
+ * 화원의 카테고리 조회를 처리하는 서비스
+ */
+public interface MemberCategoryQueryService {
+    /**
+     * 회원의 관심 카테고리 목록 조회
+     *
+     * @param memberId 회원 ID
+     * @return 회원의 카테고리 리스트
+     */
+    List<MemberCategory> findCategoriesByMember(String memberId);
+}
