497618aba2b861ddaae5806258d33745a883f504
zjhj0814 <zjhj0814@gmail.com>
2025-08-20 15:37:45 +0900

    ✨ feat: 미팅 생성/수정 요청 DTO의 String 사이즈 검증 추가

--- src/main/java/checkmo/domain/club/web/dto/meeting/MeetingRequestDTO.java
+++ src/main/java/checkmo/domain/club/web/dto/meeting/MeetingRequestDTO.java
@@ -16,15 +16,19 @@ public class MeetingRequestDTO {
     @NoArgsConstructor
     public static class MeetingCreateRequestDTO {
         @NotBlank(message = "독서모임 제목은 필수 입력입니다.")
+        @Size(max = 255, message = "독서모임 제목은 255자 이하로 입력해주세요.")
         private String title; // 독서모임 제목
         @NotNull(message = "미팅 날짜, 시간은 null이 될 수 없습니다.")
         private LocalDateTime meetingTime; // 미팅 날짜, 시간
         @NotBlank(message = "독서모임 장소는 필수 입력입니다.")
+        @Size(max = 255, message = "독서모임 장소는 255자 이하로 입력해주세요.")
         private String location; // 모임 장소
         @NotBlank(message = "독서모임 설명은 필수 입력입니다.")
+        @Size(max = 1000, message = "독서모임 설명은 1000자 이하로 입력해주세요.")
         private String content;  // 모임 내용 설명 (공지사항 내용)
+        @NotNull(message = "기수는 null이 될 수 없습니다.")
         @Min(value = 1, message = "기수는 1 이상의 정수여야 합니다.")
-        private int generation;  // 기수
+        private Integer generation;  // 기수
         @NotBlank(message = "독서모임 태그는 필수 입력입니다.")
         @Size(max = 6, message = "태그는 최대 6글자까지 입력 가능합니다.")
         private String tag; // 태그
@@ -37,15 +41,19 @@ public class MeetingRequestDTO {
     @NoArgsConstructor
     public static class MeetingUpdateRequestDTO {
         @NotBlank(message = "독서모임 제목은 필수 입력입니다.")
+        @Size(max = 255, message = "독서모임 제목은 255자 이하로 입력해주세요.")
         private String title; // 독서모임 제목
         @NotNull(message = "미팅 날짜, 시간은 null이 될 수 없습니다.")
         private LocalDateTime meetingTime; // 미팅 날짜, 시간
         @NotBlank(message = "독서모임 장소는 필수 입력입니다.")
+        @Size(max = 255, message = "독서모임 장소는 255자 이하로 입력해주세요.")
         private String location; // 모임 장소
         @NotBlank(message = "독서모임 설명은 필수 입력입니다.")
+        @Size(max = 1000, message = "독서모임 설명은 1000자 이하로 입력해주세요.")
         private String content;  // 모임 내용 설명 (공지사항 내용)
+        @NotNull(message = "기수는 null이 될 수 없습니다.")
         @Min(value = 1, message = "기수는 1 이상의 정수여야 합니다.")
-        private int generation;  // 기수
+        private Integer generation;  // 기수
         @NotBlank(message = "독서모임 태그는 필수 입력입니다.")
         @Size(max = 6, message = "태그는 최대 6글자까지 입력 가능합니다.")
         private String tag; // 태그
