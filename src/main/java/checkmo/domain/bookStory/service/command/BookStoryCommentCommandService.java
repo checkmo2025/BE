20a28e32fa957a84724177d84cd083bbd303fc5b
MODUGGAGI <kiroro0814@naver.com>
2025-08-29 21:53:02 +0900

    ✨ feat: 댓글 및 대댓글 작성 기능 구현

---
+++ src/main/java/checkmo/domain/bookStory/service/command/BookStoryCommentCommandService.java
@@ -0,0 +1,17 @@
+package checkmo.domain.bookStory.service.command;
+
+import checkmo.domain.bookStory.web.dto.BookStoryRequestDTO;
+
+public interface BookStoryCommentCommandService {
+    
+    /**
+     * 댓글/대댓글 작성
+     *
+     * @param memberId 작성자 ID
+     * @param bookStoryId 책이야기 ID
+     * @param parentCommentId 부모 댓글 ID (값이 있으면 대댓글, null이면 일반 댓글)
+     * @param request 댓글 내용
+     * @return 작성된 댓글 ID
+     */
+    Long createComment(String memberId, Long bookStoryId, Long parentCommentId, BookStoryRequestDTO.CommentCreateRequest request);
+}
---
+++ src/main/java/checkmo/domain/bookStory/service/command/BookStoryCommentCommandServiceImpl.java
@@ -0,0 +1,70 @@
+package checkmo.domain.bookStory.service.command;
+
+import checkmo.apiPayload.code.status.ErrorStatus;
+import checkmo.apiPayload.exception.GeneralException;
+import checkmo.domain.bookStory.converter.BookStoryConverter;
+import checkmo.domain.bookStory.entity.BookStory;
+import checkmo.domain.bookStory.entity.Comment;
+import checkmo.domain.bookStory.repository.CommentRepository;
+import checkmo.domain.bookStory.service.query.BookStoryQueryService;
+import checkmo.domain.bookStory.web.dto.BookStoryRequestDTO;
+import checkmo.domain.member.entity.Member;
+import checkmo.domain.member.facade.MemberQueryFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class BookStoryCommentCommandServiceImpl implements BookStoryCommentCommandService {
+
+    // Domain level 2
+    private final MemberQueryFacade memberQueryFacade;
+
+    // 자신의 QueryService
+    private final BookStoryQueryService bookStoryQueryService;
+
+    // 자신의 Repository
+    private final CommentRepository commentRepository;
+
+    @Override
+    public Long createComment(String memberId, Long bookStoryId, Long parentCommentId, BookStoryRequestDTO.CommentCreateRequest request) {
+        // 1. 책이야기 존재 여부 확인
+        BookStory bookStory = bookStoryQueryService.findBookStoryById(bookStoryId);
+        
+        // 2. 댓글 작성자 proxy 참조 조회
+        Member proxyMember = memberQueryFacade.findMemberReferenceById(memberId);
+        
+        // 3. 부모 댓글 검증 (대댓글인 경우)
+        Comment parentComment = null;
+        if (parentCommentId != null) {
+            parentComment = commentRepository.findById(parentCommentId)
+                    .orElseThrow(() -> new GeneralException(ErrorStatus.COMMENT_NOT_FOUND));
+            
+            // 부모 댓글이 같은 책이야기에 속하는지 확인
+            if (!parentComment.getBookStoryId().equals(bookStoryId)) {
+                throw new GeneralException(ErrorStatus.INVALID_PARENT_COMMENT);
+            }
+            
+            // 대댓글의 대댓글은 금지! (2단계까지만 허용)
+            if (parentComment.getParentCommentId() != null) {
+                throw new GeneralException(ErrorStatus.COMMENT_DEPTH_LIMIT_EXCEEDED);
+            }
+        }
+        
+        // 4. 댓글 생성
+        Comment comment = BookStoryConverter.fromCommentCreateRequestDTO(request, proxyMember, bookStory, parentComment);
+
+        // 5. 부모 댓글의 자식 리스트에 추가 (대댓글인 경우)
+        if (parentComment != null) {
+            parentComment.addChildComment(comment);
+        }
+
+        // 6. 댓글 저장
+        commentRepository.save(comment);
+
+        // 6. 댓글 작성된 책이야기 ID 반환
+        return bookStoryId;
+    }
+}
