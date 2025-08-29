b003dc3c18bb00cbec2aa28b67908dc56bf321a8
MODUGGAGI <kiroro0814@naver.com>
2025-08-29 21:45:54 +0900

    ✨ feat: 댓글 작성 메소드 추가 및 @Transactional 어노테이션 최상단으로 이동

--- src/main/java/checkmo/domain/bookStory/facade/BookStoryCommandFacadeImpl.java
+++ src/main/java/checkmo/domain/bookStory/facade/BookStoryCommandFacadeImpl.java
@@ -1,6 +1,7 @@
 package checkmo.domain.bookStory.facade;
 
 import checkmo.domain.bookStory.service.command.BookStoryCommandService;
+import checkmo.domain.bookStory.service.command.BookStoryCommentCommandService;
 import checkmo.domain.bookStory.service.command.BookStorySocialCommandService;
 import checkmo.domain.bookStory.web.dto.BookStoryRequestDTO;
 import lombok.RequiredArgsConstructor;
@@ -9,34 +10,36 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 public class BookStoryCommandFacadeImpl implements BookStoryCommandFacade {
 
     // 자신의 CommandService
     private final BookStoryCommandService bookStoryCommandService;
     private final BookStorySocialCommandService bookStorySocialCommandService;
+    private final BookStoryCommentCommandService bookStoryCommentCommandService;
 
     @Override
-    @Transactional
     public Long createBookStory(String memberId, BookStoryRequestDTO.BookStoryCreateRequestDTO request) {
         return bookStoryCommandService.createBookStory(memberId, request);
     }
 
     @Override
-    @Transactional
     public Long updateBookStory(String memberId, Long bookStoryId, BookStoryRequestDTO.BookStoryUpdateRequestDTO request) {
         return bookStoryCommandService.updateBookStory(memberId, bookStoryId, request);
     }
 
     @Override
-    @Transactional
     public void deleteBookStory(String memberId, Long bookStoryId) {
         bookStoryCommandService.deleteBookStory(memberId, bookStoryId);
     }
 
     @Override
-    @Transactional
     public boolean toggleLikeOnBookStory(String memberId, Long bookStoryId) {
         return bookStorySocialCommandService.toggleLikeOnBookStory(memberId, bookStoryId);
+    }
+
+    @Override
+    public Long createComment(String memberId, Long bookStoryId, Long parentCommentId, BookStoryRequestDTO.CommentCreateRequest request) {
+        return bookStoryCommentCommandService.createComment(memberId, bookStoryId, parentCommentId, request);
     }
 }
