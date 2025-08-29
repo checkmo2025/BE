391e0c7f92521871902049c784bc59d5a92a9fa1
MODUGGAGI <kiroro0814@naver.com>
2025-08-29 21:51:54 +0900

    ✨ feat: 부모 댓글 조회 메소드 추가

--- src/main/java/checkmo/domain/bookStory/repository/CommentRepository.java
+++ src/main/java/checkmo/domain/bookStory/repository/CommentRepository.java
@@ -2,6 +2,12 @@ package checkmo.domain.bookStory.repository;
 
 import checkmo.domain.bookStory.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @Query("SELECT c FROM Comment c WHERE c.bookStoryId = :bookStoryId AND c.parentComment IS NULL ORDER BY c.createdAt ASC")
+    List<Comment> findParentComments(@Param("bookStoryId") Long bookStoryId);
 }
