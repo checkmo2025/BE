5973ffda6fa798e6055381da7fc7581a04ba8116
MODUGGAGI <kiroro0814@naver.com>
2025-10-24 19:49:45 +0900

    ♻️refactor(JavaDoc): 잘못된 주석 수정
    실제로 반환 값이 책 이야기 ID 이기 때문에 주석 수정했습니다.

--- src/main/java/checkmo/domain/bookStory/service/command/BookStoryCommentCommandService.java
+++ src/main/java/checkmo/domain/bookStory/service/command/BookStoryCommentCommandService.java
@@ -11,7 +11,7 @@ public interface BookStoryCommentCommandService {
      * @param bookStoryId 책이야기 ID
      * @param parentCommentId 부모 댓글 ID (값이 있으면 대댓글, null이면 일반 댓글)
      * @param request 댓글 내용
-     * @return 작성된 댓글 ID
+     * @return 작성된 책이야기 ID
      */
     Long createComment(String memberId, Long bookStoryId, Long parentCommentId, BookStoryRequestDTO.CommentCreateRequest request);
 }
