028213f1176131ec0a1c278d923e32968925495b
MODUGGAGI <kiroro0814@naver.com>
2025-11-15 00:28:03 +0900

    feat(Member): public API로 Member 객체를 생성할 수 있는 메소드 추가
    authentication 모에서 동기적으로 Member 객체 생성을 위해 호출할 수 있는 메소드를 추가했습니다.

--- src/main/java/checkmo/member/internal/MemberAPIImpl.java
+++ src/main/java/checkmo/member/internal/MemberAPIImpl.java
@@ -6,6 +6,7 @@ import checkmo.member.internal.converter.MemberConverter;
 import checkmo.member.internal.entity.Member;
 import checkmo.member.internal.repository.projection.MemberBasicInfoProjection;
 import checkmo.member.internal.service.MemberQueryFacade;
+import checkmo.member.internal.service.command.MemberRegistrationCommandService;
 import checkmo.member.internal.service.query.MemberFollowQueryService;
 import checkmo.member.internal.service.query.MemberQueryService;
 import checkmo.member.web.dto.MemberResponseDTO;
@@ -24,9 +25,15 @@ public class MemberAPIImpl implements MemberAPI {
     // 자신의 QueryService
     private final MemberQueryService memberQueryService;
     private final MemberFollowQueryService memberFollowQueryService;
+    private final MemberRegistrationCommandService memberRegistrationCommandService;
 
     // 내부 Facade (배치 조회 로직 재사용)
     private final MemberQueryFacade memberQueryFacade;
+
+    @Override
+    public void createInitialMember(String memberId, String email) {
+        memberRegistrationCommandService.createMember(memberId, email);
+    }
 
     @Override
     public String getMemberIdByNickname(String nickname) {
