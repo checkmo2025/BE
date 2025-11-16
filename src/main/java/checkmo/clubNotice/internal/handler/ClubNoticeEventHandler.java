19ca0468cc5e12a360c80cb88cf9ffd767c5e205
MODUGGAGI <kiroro0814@naver.com>
2025-11-16 12:22:05 +0900

    refactor(clubNotice): 어노테이션 통합
    기존의 @Async, @TransactionalEventListener(phase = AFTER_COMMIT) 2개 기능을 통합한 Spring Modulith에서 공식 제공하는 어노테이션 @ApplicationModuleListener 로 어노테이션 변경하였습니다.

--- src/main/java/checkmo/clubNotice/internal/handler/ClubNoticeEventHandler.java
+++ src/main/java/checkmo/clubNotice/internal/handler/ClubNoticeEventHandler.java
@@ -1,14 +1,11 @@
 package checkmo.clubNotice.internal.handler;
 
-import static org.springframework.transaction.event.TransactionPhase.AFTER_COMMIT;
-
 import checkmo.clubMeeting.ClubMeetingEvent.ClubMeetingCreatedEvent;
 import checkmo.clubNotice.internal.service.command.ClubNoticeCommandService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.modulith.events.ApplicationModuleListener;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
@@ -17,12 +14,12 @@ public class ClubNoticeEventHandler {
 
     private final ClubNoticeCommandService clubNoticeCommandService;
 
-    @Async
-    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @ApplicationModuleListener
     public void handleClubMeetingCreatedEvent(ClubMeetingCreatedEvent event) {
         try {
             clubNoticeCommandService.createMeetingNotice(event);
         } catch (Exception e) {
+            // TODO : 재시도 로직 추가
             log.error("독서 클럽 공지 생성 실패, ClubNoticeCreatedEvent: {}", event, e);
         }
     }
