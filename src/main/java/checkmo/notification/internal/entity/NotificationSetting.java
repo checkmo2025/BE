a82a1a9d0bca68c2618bdaf8eacf80fd3863f1bf
MODUGGAGI <kiroro0814@naver.com>
2026-01-17 19:11:55 +0900

    feat(entity): NotificationSetting 엔티티 생성
    NotificationSetting 엔티티를 생성했습니다.

---
+++ src/main/java/checkmo/notification/internal/entity/NotificationSetting.java
@@ -0,0 +1,50 @@
+package checkmo.notification.internal.entity;
+
+import checkmo.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"member_id"})
+})
+public class NotificationSetting extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "member_id", nullable = false)
+    private String memberId;
+
+    @Column(name = "book_story_liked", nullable = false)
+    @Builder.Default
+    private boolean bookStoryLiked = true;
+
+    @Column(name = "book_story_comment", nullable = false)
+    @Builder.Default
+    private boolean bookStoryComment = true;
+
+    @Column(name = "club_notice_created", nullable = false)
+    @Builder.Default
+    private boolean clubNoticeCreated = true;
+
+    @Column(name = "club_meeting_created", nullable = false)
+    @Builder.Default
+    private boolean clubMeetingCreated = true;
+
+}
