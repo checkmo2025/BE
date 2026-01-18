563575e402c98aabbccdb3a0a2258be977112a2b
MODUGGAGI <kiroro0814@naver.com>
2026-01-18 14:30:48 +0900

    feat(service): 알림 설정 변경 메소드 구현
    특정 사용자의 알림 설정을 변경할 수 있는 메소드를 구현했습니다.

--- src/main/java/checkmo/notification/internal/service/command/NotificationSettingCommandService.java
+++ src/main/java/checkmo/notification/internal/service/command/NotificationSettingCommandService.java
@@ -1,6 +1,9 @@
 package checkmo.notification.internal.service.command;
 
 import checkmo.notification.internal.entity.NotificationSetting;
+import checkmo.notification.web.dto.NotificationSettingType;
+import checkmo.notification.internal.exception.NotificationErrorStatus;
+import checkmo.notification.internal.exception.NotificationException;
 import checkmo.notification.internal.repository.NotificationSettingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,5 +26,17 @@ public class NotificationSettingCommandService {
                 .build();
 
         notificationSettingRepository.save(notificationSetting);
+    }
+
+    public void toggleNotificationSetting(String memberId, NotificationSettingType settingType) {
+        NotificationSetting setting = notificationSettingRepository.findByMemberId(memberId)
+                .orElseThrow(() -> new NotificationException(NotificationErrorStatus.NOTIFICATION_SETTING_NOT_FOUND));
+
+        switch (settingType) {
+            case BOOK_STORY_LIKED -> setting.toggleBookStoryLiked();
+            case BOOK_STORY_COMMENT -> setting.toggleBookStoryComment();
+            case CLUB_NOTICE_CREATED -> setting.toggleClubNoticeCreated();
+            case CLUB_MEETING_CREATED -> setting.toggleClubMeetingCreated();
+        }
     }
 }
