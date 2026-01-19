2e122a08449fee571f358f87915d7b6b7975cbae
MODUGGAGI <kiroro0814@naver.com>
2026-01-19 13:53:22 +0900

    feat(service): 정기모임 생성 시 이벤트 발행 구현
    정기모임이 생성되면 이벤트를 발행하는 기능을 추가했습니다.

--- src/main/java/checkmo/clubMeeting/ClubMeetingEvent.java
+++ src/main/java/checkmo/clubMeeting/ClubMeetingEvent.java
@@ -3,8 +3,14 @@ package checkmo.clubMeeting;
 import lombok.Builder;
 
 public class ClubMeetingEvent {
+
+    //TODO : 아래 두개의 이벤트 이름 수정 필요!!
+
     @Builder
     public record ClubMeetingCreatedEvent(Long clubId, Long meetingId, Long version, String title, String content) {
     }
 
+    @Builder
+    public record ClubMeetingCreated(Long eventId, Long clubId, String clubName) {
+    }
 }
