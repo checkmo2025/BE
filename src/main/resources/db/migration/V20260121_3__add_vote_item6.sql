4e584b9c59d203f1a7d3a99a7308cc93f2e38da6
zjhj0814 <zjhj0814@gmail.com>
2026-01-21 23:49:07 +0900

    fix(flyway): club_member_vote 새로운 컬럼에 대해 기존 데이터는 0으로 채우기

--- src/main/resources/db/migration/V20260121_3__add_vote_item6.sql
+++ src/main/resources/db/migration/V20260121_3__add_vote_item6.sql
@@ -2,4 +2,11 @@ ALTER TABLE vote
     ADD COLUMN item6 VARCHAR(255);
 
 ALTER TABLE club_member_vote
-    ADD COLUMN item6 BIT NOT NULL;
+    ADD COLUMN item6 BIT NULL;
+
+UPDATE club_member_vote
+    SET item6 = b'0'
+        WHERE item6 IS NULL;
+
+ALTER TABLE club_member_vote
+    MODIFY COLUMN item6 BIT NOT NULL;
