2c035690aab5ce70eae6dbb0ba3070a62abbe968
MODUGGAGI <kiroro0814@naver.com>
2026-01-21 15:21:59 +0900

    feat(application): 비동기 활성화
    애플리케이션에서 비동기 로직이 돌아가게 활성화 시켰습니다.

--- src/main/java/checkmo/CheckmoApplication.java
+++ src/main/java/checkmo/CheckmoApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 
+@EnableAsync
 @EnableRetry
 @EnableScheduling
 @EnableJpaAuditing
