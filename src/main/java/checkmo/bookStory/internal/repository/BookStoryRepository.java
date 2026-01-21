7aca60111d2aed7ad9c6e271f503345427d35ac0
MODUGGAGI <kiroro0814@naver.com>
2026-01-21 15:20:12 +0900

    feat(repository): 책이야기 조회수 증가 메소드 구현
    repository 계층에 select 쿼리 없이 바로 update 쿼리를 통해 조회 수를 증가시키는 메소드를 구현했습니다.

--- src/main/java/checkmo/bookStory/internal/repository/BookStoryRepository.java
+++ src/main/java/checkmo/bookStory/internal/repository/BookStoryRepository.java
@@ -2,6 +2,13 @@ package checkmo.bookStory.internal.repository;
 
 import checkmo.bookStory.internal.entity.BookStory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BookStoryRepository extends JpaRepository<BookStory, Long>, BookStoryQueryRepository {
+
+    @Modifying
+    @Query("UPDATE BookStory b SET b.viewCount = b.viewCount + :viewCount WHERE b.id = :id")
+    void incrementViewCount(@Param("id") Long bookStoryId, @Param("viewCount") int viewCount);
 }
