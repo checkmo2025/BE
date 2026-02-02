8cab3b1ee7601b7f024c60521d6e2013c2e1c985
MODUGGAGI <kiroro0814@naver.com>
2026-02-02 22:55:25 +0900

    feat(service): 신고 조회 기능 추가
    관리자용 신고 조회 기능을 추가했습니다.

--- src/main/java/checkmo/member/internal/repository/MemberReportRepository.java
+++ src/main/java/checkmo/member/internal/repository/MemberReportRepository.java
@@ -1,7 +1,12 @@
 package checkmo.member.internal.repository;
 
 import checkmo.member.internal.entity.MemberReport;
+import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberReportRepository extends JpaRepository<MemberReport, Long> {
+
+    @EntityGraph(attributePaths = {"reportedMember"})
+    List<MemberReport> findByReportedMemberNickName(String nickName);
 }
