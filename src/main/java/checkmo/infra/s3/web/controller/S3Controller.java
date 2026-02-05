e470d78c9c77ccc2ca6688017f05e6da68ef86ed
MODUGGAGI <kiroro0814@naver.com>
2026-02-05 23:28:46 +0900

    refactor(controller): Controller 분리
    controller의 메소드를 각각의 이미지에 맞게끔 분리시켰습니다.

--- src/main/java/checkmo/infra/s3/web/controller/S3Controller.java
+++ src/main/java/checkmo/infra/s3/web/controller/S3Controller.java
@@ -1,6 +1,7 @@
 package checkmo.infra.s3.web.controller;
 
 import checkmo.common.apiPayload.ApiResponse;
+import checkmo.infra.s3.internal.entity.FileUploadType;
 import checkmo.infra.s3.internal.service.S3Service;
 import checkmo.infra.s3.web.dto.S3RequestDTO;
 import checkmo.infra.s3.web.dto.S3ResponseDTO;
@@ -22,17 +23,57 @@ public class S3Controller {
 
     private final S3Service s3Service;
 
-    @Operation(summary = "이미지용 presigned URL 발급 API", description = "프로필 이미지/독서 클럽/클럽 공지사항의 이미지 업로드를 위한 S3 presigned URL을 발급합니다.")
+    @Operation(summary = "프로필 이미지 업로드 URL 발급", description = "회원 프로필 이미지 업로드를 위한 URL을 발급합니다.")
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "로그인이 필요한 서비스 입니다.")
     })
-    @PostMapping("/image/upload-url")
+    @PostMapping("/profile/upload-url")
     public ApiResponse<S3ResponseDTO.PresignedUrl> getProfileImageUploadUrl(
             @Valid @RequestBody S3RequestDTO.ImageUpload request
     ) {
         return ApiResponse.onSuccess(
-                s3Service.generatePresignedUploadUrl(request.getOriginalFileName(), request.getContentType()));
+                s3Service.generatePresignedUploadUrl(
+                        request.getOriginalFileName(),
+                        request.getContentType(),
+                        FileUploadType.PROFILE
+                ));
+    }
+
+    @Operation(summary = "클럽 대표 이미지 업로드 URL 발급", description = "독서 클럽 대표 이미지 업로드를 위한 URL을 발급합니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "로그인이 필요한 서비스 입니다.")
+    })
+    @PostMapping("/club/upload-url")
+    public ApiResponse<S3ResponseDTO.PresignedUrl> getClubImageUploadUrl(
+            @Valid @RequestBody S3RequestDTO.ImageUpload request
+    ) {
+        return ApiResponse.onSuccess(
+                s3Service.generatePresignedUploadUrl(
+                        request.getOriginalFileName(),
+                        request.getContentType(),
+                        FileUploadType.CLUB
+                ));
+    }
+
+    @Operation(summary = "공지사항 이미지 업로드 URL 발급", description = "공지사항에 들어갈 이미지 업로드를 위한 URL을 발급합니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "로그인이 필요한 서비스 입니다.")
+    })
+    @PostMapping("/notice/upload-url")
+    public ApiResponse<S3ResponseDTO.PresignedUrl> getNoticeImageUploadUrl(
+            @Valid @RequestBody S3RequestDTO.ImageUpload request
+    ) {
+        return ApiResponse.onSuccess(
+                s3Service.generatePresignedUploadUrl(
+                        request.getOriginalFileName(),
+                        request.getContentType(),
+                        FileUploadType.NOTICE
+                ));
     }
 }
