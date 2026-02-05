7fcdb4a1ff6153ce50a649711408437096800f0d
MODUGGAGI <kiroro0814@naver.com>
2026-02-05 23:54:01 +0900

    refactor(controller): endpoint 변경
    이미지 저장하는 controller의 endpoint를 변경하였습니다.

--- src/main/java/checkmo/infra/s3/web/controller/S3Controller.java
+++ src/main/java/checkmo/infra/s3/web/controller/S3Controller.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/s3")
+@RequestMapping("/api/image")
 @RequiredArgsConstructor
 @Tag(name = "S3 Image", description = "S3에 저장할 이미지에 대해서 presigned URL을 발급하는 API")
 public class S3Controller {
