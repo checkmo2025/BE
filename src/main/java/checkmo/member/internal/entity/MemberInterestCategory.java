b1126c69baea65f5288d787881fefdc56a177d1e
zjhj0814 <zjhj0814@gmail.com>
2026-02-07 22:19:52 +0900

    refactor(MemberInterestCategory): 카테고리 이름 수정 & 카테고리 설명 추가
    - CHILDREN -> CHILDREN_BOOKS
    - DOMESTIC -> RELIGION_PHILOSOPHY

--- src/main/java/checkmo/member/internal/entity/MemberInterestCategory.java
+++ src/main/java/checkmo/member/internal/entity/MemberInterestCategory.java
@@ -1,19 +1,29 @@
 package checkmo.member.internal.entity;
 
 public enum MemberInterestCategory {
-    DOMESTIC,
-    FICTION_POETRY_DRAMA,
-    ESSAY,
-    ECONOMY_MANAGEMENT,
-    SELF_DEVELOPMENT,
-    HUMANITIES,
-    TRAVEL,
-    HISTORY_CULTURE,
-    SOCIAL_SCIENCE,
-    POLITICS_DIPLOMACY_DEFENSE,
-    COMPUTER_IT,
-    SCIENCE,
-    FOREIGN_LANGUAGE,
-    ART_POP_CULTURE,
-    CHILDREN
+    FICTION_POETRY_DRAMA("소설/시/희곡"),
+    ESSAY("에세이"),
+    HUMANITIES("인문학"),
+    SOCIAL_SCIENCE("사회 과학"),
+    POLITICS_DIPLOMACY_DEFENSE("정치/외교"),
+    ECONOMY_MANAGEMENT("경제/경영"),
+    SELF_DEVELOPMENT("자기 계발"),
+    HISTORY_CULTURE("역사/문화"),
+    SCIENCE("과학"),
+    COMPUTER_IT("컴퓨터/IT"),
+    ART_POP_CULTURE("예술/대중 문화"),
+    TRAVEL("여행"),
+    FOREIGN_LANGUAGE("외국어"),
+    CHILDREN_BOOKS("어린이 도서"),
+    RELIGION_PHILOSOPHY("종교/철학");
+
+    private final String description;
+
+    MemberInterestCategory(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
