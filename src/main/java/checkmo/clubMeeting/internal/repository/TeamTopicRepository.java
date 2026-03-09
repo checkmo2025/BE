e11793edcbe3093147be18e7ea65b695b10a9f37
zjhj0814 <zjhj0814@gmail.com>
2026-03-09 20:17:41 +0900

    fix(clubMeeting): 발제 선택 경쟁 가짜 성공 해결

--- src/main/java/checkmo/clubMeeting/internal/repository/TeamTopicRepository.java
+++ src/main/java/checkmo/clubMeeting/internal/repository/TeamTopicRepository.java
@@ -1,10 +1,11 @@
 package checkmo.clubMeeting.internal.repository;
 
 import checkmo.clubMeeting.internal.entity.TeamTopic;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface TeamTopicRepository extends JpaRepository<TeamTopic, Long> {
     Optional<TeamTopic> findByTeamIdAndTopicId(Long teamId, Long topicId);
@@ -14,4 +15,6 @@ public interface TeamTopicRepository extends JpaRepository<TeamTopic, Long> {
             + "WHERE tt.team.id = :teamId "
             + "AND tt.topic.id IN :topicIds")
     List<Long> findTopicIdsByTeamIdAndTopicIds(Long teamId, List<Long> topicIds);
+
+    boolean existsByTeamIdAndTopicId(Long teamId, Long topicId);
 }
