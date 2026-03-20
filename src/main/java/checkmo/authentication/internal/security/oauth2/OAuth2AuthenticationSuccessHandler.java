9145ea8eb8521360ae49743b397f47b34a11a647
hyunil <yhi9839@gmail.com>
2026-03-20 19:24:52 +0900

    hotfix(OAuthHandler) : 소셜 로그인 성공 후 path 변경

--- src/main/java/checkmo/authentication/internal/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/checkmo/authentication/internal/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -39,7 +39,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
         AuthUser user = principalDetails.getUser();
 
-        String path = user.isProfileCompleted() ? "home" : "profile";
+        String path = user.isProfileCompleted() ? "/" : "/signup/terms";
 
         // 기본 리다이렉트 URI
         String targetUrl = UriComponentsBuilder.fromUriString(baseUri)
